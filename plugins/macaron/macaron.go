1baba9102b6328639fbcc41c411ec050ab9b75a5
Mike Goldsmth <goldsmith.mike@gmail.com>
2020-04-24 16:16:30 +0100

    add macaron/v1 plugin

---
+++ plugins/macaron/macaron.go
@@ -0,0 +1,79 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package macaron
+
+import (
+	"fmt"
+	"net/http"
+
+	"gopkg.in/macaron.v1"
+
+	"go.opentelemetry.io/contrib/internal/trace"
+	otelglobal "go.opentelemetry.io/otel/api/global"
+	otelpropagation "go.opentelemetry.io/otel/api/propagation"
+	oteltrace "go.opentelemetry.io/otel/api/trace"
+)
+
+const (
+	tracerKey  = "otel-go-contrib-tracer"
+	tracerName = "go.opentelemetry.io/contrib/plugins/macaron"
+)
+
+// Middleware returns stuff
+func Middleware(service string, opts ...Option) macaron.Handler {
+	cfg := Config{}
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+	if cfg.Tracer == nil {
+		cfg.Tracer = otelglobal.Tracer(tracerName)
+	}
+	if cfg.Propagators == nil {
+		cfg.Propagators = otelglobal.Propagators()
+	}
+	return func(res http.ResponseWriter, req *http.Request, c *macaron.Context) {
+		savedCtx := c.Req.Request.Context()
+		defer func() {
+			c.Req.Request = c.Req.Request.WithContext(savedCtx)
+		}()
+
+		ctx := otelpropagation.ExtractHTTP(savedCtx, cfg.Propagators, c.Req.Header)
+		opts := []oteltrace.StartOption{
+			oteltrace.WithAttributes(trace.NetAttributesFromHTTPRequest("tcp", c.Req.Request)...),
+			oteltrace.WithAttributes(trace.EndUserAttributesFromHTTPRequest(c.Req.Request)...),
+			oteltrace.WithAttributes(trace.HTTPServerAttributesFromHTTPRequest(service, c.Req.RequestURI, c.Req.Request)...),
+			oteltrace.WithSpanKind(oteltrace.SpanKindServer),
+		}
+		// TODO: span name should be router template not the actual request path, eg /user/:id vs /user/123
+		spanName := c.Req.RequestURI
+		if spanName == "" {
+			spanName = fmt.Sprintf("HTTP %s route not found", c.Req.Method)
+		}
+		ctx, span := cfg.Tracer.Start(ctx, spanName, opts...)
+		defer span.End()
+
+		// pass the span through the request context
+		c.Req.Request = c.Req.Request.WithContext(ctx)
+
+		// serve the request to the next middleware
+		c.Next()
+
+		status := c.Resp.Status()
+		attrs := trace.HTTPAttributesFromHTTPStatusCode(status)
+		spanStatus, spanMessage := trace.SpanStatusFromHTTPStatusCode(status)
+		span.SetAttributes(attrs...)
+		span.SetStatus(spanStatus, spanMessage)
+	}
+}
