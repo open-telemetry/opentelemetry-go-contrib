7a7679d2af73885957b02f746bcaaa4c10546c95
Krzesimir Nowak <krzesimir@kinvolk.io>
2020-05-04 19:28:45 +0200

    gorilla/mux instrumentation (#19)
    * Add an updated copy of otel-go mock tracer
    
    * Add functions for getting attributes from HTTP requests
    
    * Add gorilla mux instrumentation
    
    * Add example for gorilla mux instrumentation
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>

---
+++ plugins/gorilla/mux/example/server.go
@@ -0,0 +1,75 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	muxtrace "go.opentelemetry.io/contrib/plugins/gorilla/mux"
+	otelglobal "go.opentelemetry.io/otel/api/global"
+	otelkey "go.opentelemetry.io/otel/api/key"
+	oteltrace "go.opentelemetry.io/otel/api/trace"
+	oteltracestdout "go.opentelemetry.io/otel/exporters/trace/stdout"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+var tracer = otelglobal.Tracer("mux-server")
+
+func main() {
+	initTracer()
+	r := mux.NewRouter()
+	r.Use(muxtrace.Middleware("my-server"))
+	r.HandleFunc("/users/{id:[0-9]+}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		name := getUser(r.Context(), id)
+		reply := fmt.Sprintf("user %s (id %s)\n", name, id)
+		_, _ = w.Write(([]byte)(reply))
+	}))
+	http.Handle("/", r)
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func initTracer() {
+	exporter, err := oteltracestdout.NewExporter(oteltracestdout.Options{PrettyPrint: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg := sdktrace.Config{
+		DefaultSampler: sdktrace.AlwaysSample(),
+	}
+	tp, err := sdktrace.NewProvider(
+		sdktrace.WithConfig(cfg),
+		sdktrace.WithSyncer(exporter),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	otelglobal.SetTraceProvider(tp)
+}
+
+func getUser(ctx context.Context, id string) string {
+	_, span := tracer.Start(ctx, "getUser", oteltrace.WithAttributes(otelkey.String("id", id)))
+	defer span.End()
+	if id == "123" {
+		return "muxtrace tester"
+	}
+	return "unknown"
+}
