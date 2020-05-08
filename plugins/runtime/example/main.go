e92ea3b2d3c343de8c01004c4c8d2c07b4ece12e
Chris Leavoy <chris@sdk.ca>
2020-05-08 11:14:33 -0400

    Runtime metrics plugin (#9)
    * stub runtime go func metrics
    
    * stub more runtime metrics
    
    * fix go mod path
    
    * bump 1.14, remove procstats stub
    
    * cleanup go runtime metrics
    
    * add rwmutex
    
    * Update plugins/metrics/runtime/example/main.go
    
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>
    
    * Update plugins/metrics/runtime/runtime_test.go
    
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>
    
    * Update plugins/metrics/runtime/runtime.go
    
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>
    
    * add doc.go, pr feedback
    
    * plugins/metrics/runtime -> plugins/runtime
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

---
+++ plugins/runtime/example/main.go
@@ -0,0 +1,59 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"go.opentelemetry.io/otel/api/global"
+	metricstdout "go.opentelemetry.io/otel/exporters/metric/stdout"
+	"go.opentelemetry.io/otel/sdk/metric/controller/push"
+
+	"go.opentelemetry.io/contrib/plugins/runtime"
+)
+
+func initMeter() *push.Controller {
+	pusher, err := metricstdout.NewExportPipeline(metricstdout.Config{
+		Quantiles:   []float64{0.5},
+		PrettyPrint: true,
+	}, 10*time.Second)
+	if err != nil {
+		log.Panicf("failed to initialize metric stdout exporter %v", err)
+	}
+	global.SetMeterProvider(pusher)
+	return pusher
+}
+
+func main() {
+	defer initMeter().Stop()
+
+	meter := global.Meter("runtime")
+
+	r := runtime.New(meter, time.Second)
+	err := r.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGTERM, syscall.SIGINT)
+	<-stopChan
+
+	r.Stop()
+}
