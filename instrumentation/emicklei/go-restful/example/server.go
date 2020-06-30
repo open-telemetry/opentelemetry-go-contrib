2f81d6059153cb1c922f97df77d4217f1e3dc5c5
Evan Torrie <evantorrie@users.noreply.github.com>
2020-06-29 22:51:09 -0700

    Adds a go-restful trace instrumenter via a FilterFunc
    A filter may be added at any one of the following:
       * container scope
       * webservice scope
       * route scope
    
    Includes a docker-compose example with instructions.

---
+++ instrumentation/emicklei/go-restful/example/server.go
@@ -0,0 +1,97 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/emicklei/go-restful/v3"
+	restfultrace "go.opentelemetry.io/contrib/instrumentation/emicklei/go-restful"
+	otelglobal "go.opentelemetry.io/otel/api/global"
+	otelkv "go.opentelemetry.io/otel/api/kv"
+	oteltrace "go.opentelemetry.io/otel/api/trace"
+	oteltracestdout "go.opentelemetry.io/otel/exporters/trace/stdout"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+var tracer oteltrace.Tracer
+
+type UserResource struct {}
+
+func (u UserResource) WebService() *restful.WebService {
+	ws := &restful.WebService{}
+
+	ws.
+		Path("/users").
+		Consumes(restful.MIME_JSON).
+		Produces(restful.MIME_JSON)
+
+	ws.Route(ws.GET("/{user-id}").To(u.getUser).
+		Param(ws.PathParameter("user-id", "identifier of the user").DataType("integer").DefaultValue("1")).
+		Writes(User{}). // on the response
+		Returns(200, "OK", User{}).
+		Returns(404, "Not Found", nil))
+	return ws;
+}
+
+func main() {
+	initTracer()
+	u := UserResource{}
+	// create the Otel filter
+	filter := restfultrace.OtelFilter("my-service")
+	// use it
+	restful.DefaultContainer.Filter(filter)
+	restful.DefaultContainer.Add(u.WebService())
+
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func initTracer() {
+	exporter, err := oteltracestdout.NewExporter(oteltracestdout.Options{PrettyPrint: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg := sdktrace.Config{
+		DefaultSampler: sdktrace.AlwaysSample(),
+	}
+	tp, err := sdktrace.NewProvider(
+		sdktrace.WithConfig(cfg),
+		sdktrace.WithSyncer(exporter),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	otelglobal.SetTraceProvider(tp)
+	tracer = otelglobal.TraceProvider().Tracer("go-restful-server", oteltrace.WithInstrumentationVersion("0.1"))
+}
+
+func (u UserResource) getUser(req *restful.Request, resp *restful.Response) {
+	uid := req.PathParameter("user-id")
+	_, span := tracer.Start(req.Request.Context(), "getUser", oteltrace.WithAttributes(otelkv.String("id", uid)))
+	defer span.End()
+	id, err := strconv.Atoi(uid)
+	if err == nil && id >= 100 {
+		_ = resp.WriteEntity(User{id})
+		return
+	}
+	_ = resp.WriteErrorString(http.StatusNotFound, "User could not be found.")
+	return
+}
+
+type User struct {
+	ID int  `json:"id" description:"identifier of the user"`
+}
---
+++ instrumentation/emicklei/go-restful/go.mod
@@ -0,0 +1,13 @@
+module go.opentelemetry.io/contrib/instrumentation/emicklei/go-restful
+
+go 1.14
+
+replace go.opentelemetry.io/contrib => ../../..
+
+require (
+	github.com/emicklei/go-restful/v3 v3.0.0
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/stretchr/testify v1.6.1
+	go.opentelemetry.io/contrib v0.7.0
+	go.opentelemetry.io/otel v0.7.0
+)
