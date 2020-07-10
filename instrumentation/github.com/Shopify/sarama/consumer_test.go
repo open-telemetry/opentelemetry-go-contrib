5eaaf17b0475c591603f080a5e01c4d1cbb0c1f0
Sam Xie <xsambundy@gmail.com>
2020-07-10 15:00:10 +0800

    Add test for sarama consumer

---
+++ instrumentation/github.com/Shopify/sarama/consumer_test.go
@@ -0,0 +1,168 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sarama
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/Shopify/sarama"
+	"github.com/Shopify/sarama/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/kv"
+	"go.opentelemetry.io/otel/api/propagation"
+	"go.opentelemetry.io/otel/api/standard"
+	"go.opentelemetry.io/otel/api/trace"
+
+	mocktracer "go.opentelemetry.io/contrib/internal/trace"
+)
+
+const (
+	serviceName = "test-service-name"
+	topic       = "test-topic"
+)
+
+var (
+	propagators = global.Propagators()
+)
+
+func TestWrapPartitionConsumer(t *testing.T) {
+	// Mock tracer
+	mt := mocktracer.NewTracer("kafka")
+
+	// Mock partition consumer controller
+	consumer := mocks.NewConsumer(t, sarama.NewConfig())
+	mockPartitionConsumer := consumer.ExpectConsumePartition(topic, 0, 0)
+
+	// Create partition consumer
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, 0)
+	assert.NoError(t, err)
+
+	partitionConsumer = WrapPartitionConsumer(serviceName, partitionConsumer, WithTracer(mt))
+
+	consumeAndCheck(t, mt, mockPartitionConsumer, partitionConsumer)
+}
+
+func TestWrapConsumer(t *testing.T) {
+	// Mock tracer
+	mt := mocktracer.NewTracer("kafka")
+
+	// Mock partition consumer controller
+	mockConsumer := mocks.NewConsumer(t, sarama.NewConfig())
+	mockPartitionConsumer := mockConsumer.ExpectConsumePartition(topic, 0, 0)
+
+	// Wrap consumer
+	consumer := WrapConsumer(serviceName, mockConsumer, WithTracer(mt))
+
+	// Create partition consumer
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, 0)
+	assert.NoError(t, err)
+
+	consumeAndCheck(t, mt, mockPartitionConsumer, partitionConsumer)
+}
+
+func consumeAndCheck(t *testing.T, mt *mocktracer.Tracer, mockPartitionConsumer *mocks.PartitionConsumer, partitionConsumer sarama.PartitionConsumer) {
+	// Create message with span context
+	ctx, _ := mt.Start(context.Background(), "")
+	message := sarama.ConsumerMessage{Key: []byte("foo")}
+	propagation.InjectHTTP(ctx, propagators, NewConsumerMessageCarrier(&message))
+
+	// Produce message
+	mockPartitionConsumer.YieldMessage(&message)
+	mockPartitionConsumer.YieldMessage(&sarama.ConsumerMessage{Key: []byte("foo2")})
+
+	// Consume messages
+	msgList := make([]*sarama.ConsumerMessage, 2)
+	msgList[0] = <-partitionConsumer.Messages()
+	msgList[1] = <-partitionConsumer.Messages()
+	assert.NoError(t, partitionConsumer.Close())
+	// Wait for the channel to be closed
+	<-partitionConsumer.Messages()
+
+	// Check spans length
+	spans := mt.EndedSpans()
+	assert.Len(t, spans, 2)
+
+	expectedList := []struct {
+		kvList []kv.KeyValue
+		links  map[trace.SpanContext][]kv.KeyValue
+		kind   trace.SpanKind
+		msgKey []byte
+	}{
+		{
+			kvList: []kv.KeyValue{
+				standard.ServiceNameKey.String(serviceName),
+				standard.MessagingSystemKey.String("kafka"),
+				standard.MessagingDestinationKindKeyTopic,
+				standard.MessagingDestinationKey.String("test-topic"),
+				standard.MessagingOperationReceive,
+				standard.MessagingMessageIDKey.Int64(1),
+				kafkaPartitionKey.Int32(0),
+			},
+			links: map[trace.SpanContext][]kv.KeyValue{
+				trace.SpanFromContext(ctx).SpanContext(): nil,
+			},
+			kind:   trace.SpanKindConsumer,
+			msgKey: []byte("foo"),
+		},
+		{
+			kvList: []kv.KeyValue{
+				standard.ServiceNameKey.String(serviceName),
+				standard.MessagingSystemKey.String("kafka"),
+				standard.MessagingDestinationKindKeyTopic,
+				standard.MessagingDestinationKey.String("test-topic"),
+				standard.MessagingOperationReceive,
+				standard.MessagingMessageIDKey.Int64(2),
+				kafkaPartitionKey.Int32(0),
+			},
+			links:  make(map[trace.SpanContext][]kv.KeyValue),
+			kind:   trace.SpanKindConsumer,
+			msgKey: []byte("foo2"),
+		},
+	}
+
+	for i, expected := range expectedList {
+		t.Run(fmt.Sprint("index", i), func(t *testing.T) {
+			span := spans[i]
+
+			assert.Equal(t, expected.links, span.Links)
+
+			remoteSpanFromMessage := trace.RemoteSpanContextFromContext(propagation.ExtractHTTP(context.Background(), propagators, NewConsumerMessageCarrier(msgList[i])))
+			assert.Equal(t, span.SpanContext(), remoteSpanFromMessage,
+				"span context should be injected into the consumer message headers")
+
+			assert.Equal(t, "kafka.consume", span.Name)
+			assert.Equal(t, expected.kind, span.Kind)
+			assert.Equal(t, expected.msgKey, msgList[i].Key)
+			for _, k := range expected.kvList {
+				assert.Equal(t, k.Value, span.Attributes[k.Key], k.Key)
+			}
+		})
+	}
+}
+
+func TestConsumer_ConsumePartitionWithError(t *testing.T) {
+	// Mock partition consumer controller
+	mockConsumer := mocks.NewConsumer(t, sarama.NewConfig())
+	mockConsumer.ExpectConsumePartition(topic, 0, 0)
+
+	consumer := WrapConsumer(serviceName, mockConsumer)
+	consumer.ConsumePartition(topic, 0, 0)
+	// Consume twice
+	_, err := consumer.ConsumePartition(topic, 0, 0)
+	assert.Error(t, err)
+}
--- instrumentation/github.com/Shopify/sarama/go.mod
+++ instrumentation/github.com/Shopify/sarama/go.mod
@@ -5,7 +5,10 @@ go 1.14
 replace go.opentelemetry.io/contrib => ../../../..
 
 require (
-    github.com/Shopify/sarama v1.26.4
-    github.com/google/uuid v1.1.1
-    go.opentelemetry.io/otel v0.7.0
+	github.com/Shopify/sarama v1.26.4
+	github.com/google/uuid v1.1.1
+	github.com/stretchr/testify v1.6.1
+	go.opentelemetry.io/contrib v0.7.0
+	go.opentelemetry.io/otel v0.7.0
+	google.golang.org/grpc v1.30.0
 )
