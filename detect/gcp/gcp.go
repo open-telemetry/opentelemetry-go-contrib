fec2bf5ec203eccece847b8b0fcf57e451d5d8da
YANYZP <yzp@google.com>
2020-07-10 16:03:10 -0400

    # This is a combination of 4 commits. # This is the 1st commit message:
    first
    
    # The commit message #2 will be skipped:
    
    # gcp detector
    
    # The commit message #3 will be skipped:
    
    # Bump go.mongodb.org/mongo-driver from 1.3.4 to 1.3.5 in /instrumentation/go.mongodb.org/mongo-driver (#124)
    #
    # Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    # Co-authored-by: dependabot[bot] <dependabot[bot]@users.noreply.github.com>
    
    # The commit message #4 will be skipped:
    
    # Bump github.com/golangci/golangci-lint from 1.28.1 to 1.28.2 in /tools (#125)
    #
    # Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    # Co-authored-by: dependabot[bot] <dependabot[bot]@users.noreply.github.com>
    # Co-authored-by: Liz Fong-Jones <lizf@honeycomb.io>

---
+++ detect/gcp/gcp.go
@@ -0,0 +1,90 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcp
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"cloud.google.com/go/compute/metadata"
+
+	"go.opentelemetry.io/otel/api/kv"
+	"go.opentelemetry.io/otel/api/standard"
+	"go.opentelemetry.io/otel/sdk/resource"
+)
+
+// GCP is a detector that implements Detect() function
+type GCP struct{}
+
+// Detect detects associated resources when running on GCE hosts.
+func (gcp *GCP) Detect(ctx context.Context) (*resource.Resource, error) {
+	if !metadata.OnGCE() {
+		return nil, nil
+	}
+
+	labels := []kv.KeyValue{
+		standard.CloudProviderGCP,
+		standard.CloudRegionKey.String(""),
+	}
+
+	projectID, err := metadata.ProjectID()
+	logError(err)
+	if projectID != "" {
+		labels = append(labels, standard.CloudAccountIDKey.String(projectID))
+	}
+
+	zone, err := metadata.Zone()
+	logError(err)
+	if zone != "" {
+		labels = append(labels, standard.CloudZoneKey.String(zone))
+	}
+
+	instanceID, err := metadata.InstanceID()
+	logError(err)
+	if instanceID != "" {
+		labels = append(labels, standard.HostIDKey.String(instanceID))
+	}
+
+	name, err := metadata.InstanceName()
+	logError(err)
+	if instanceID != "" {
+		labels = append(labels, standard.HostNameKey.String(name))
+	}
+
+	hostname, err := metadata.Hostname()
+	logError(err)
+	if instanceID != "" {
+		labels = append(labels, standard.HostHostNameKey.String(hostname))
+	}
+
+	hostType, err := metadata.Get("instance/machine-type")
+	logError(err)
+	if instanceID != "" {
+		labels = append(labels, standard.HostTypeKey.String(hostType))
+	}
+
+	return resource.New(labels...), nil
+
+}
+
+//logError logs error only if the error is present and it is not 'not defined'
+func logError(err error) {
+	if err != nil {
+		if !strings.Contains(err.Error(), "not defined") {
+			log.Printf("Error retrieving gcp metadata: %v", err)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go.opentelemetry.io/contrib
 go 1.14
 
 require (
+	cloud.google.com/go v0.26.0
 	go.opentelemetry.io/otel v0.7.0
 	google.golang.org/grpc v1.30.0
 )
