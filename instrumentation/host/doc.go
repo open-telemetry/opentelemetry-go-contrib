30cd85cbac211f35e2aa10b68758f5af5b001f49
jmacd <jmacd@lightstep.com>
2020-08-16 00:16:42 -0700

    Similar to runtime

---
+++ instrumentation/host/doc.go
@@ -0,0 +1,24 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// package host implements the work-in-progress conventional host
+// metrics specified by OpenTelemetry.  Host metrics are sometimes
+// collected through the OpenTelemetry Collector "hostmetrics"
+// receiver running as an agent; this instrumentation is an
+// alternative for processes that want to record the same information
+// with am agent.
+//
+// The metrics produced are:
+// @@@
+package runtime // import "go.opentelemetry.io/contrib/instrumentation/host"
