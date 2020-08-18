8d5f38c5bfd56a9204b8704351db436d1e7adf60
reggiemcdonald <regmcdonald@google.com>
2020-08-18 21:23:02 +0000

    fix: remove tracing on client

--- instrumentation/github.com/astaxie/beego/example/client/client.go
+++ instrumentation/github.com/astaxie/beego/example/client/client.go
@@ -15,80 +15,36 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 
 	"net/http"
-	"time"
-
-	otelhttp "go.opentelemetry.io/contrib/instrumentation/net/http"
-
-	"go.opentelemetry.io/otel/api/correlation"
-	"go.opentelemetry.io/otel/api/global"
-	"go.opentelemetry.io/otel/api/kv"
-	"go.opentelemetry.io/otel/api/standard"
-	"go.opentelemetry.io/otel/api/trace"
-	"go.opentelemetry.io/otel/exporters/stdout"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func initTracer() {
-	// Create stdout exporter to be able to retrieve
-	// the collected spans.
-	exporter, err := stdout.NewExporter(stdout.WithPrettyPrint())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
-	// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
-	tp, err := sdktrace.NewProvider(sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
-		sdktrace.WithSyncer(exporter))
-	if err != nil {
-		log.Fatal(err)
-	}
-	global.SetTraceProvider(tp)
-}
-
 func main() {
-	initTracer()
 	url := flag.String("server", "http://localhost:7777/hello", "server url")
 	flag.Parse()
 
-	client := http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+	client := http.Client{}
 
-	ctx := correlation.NewContext(context.Background(),
-		kv.String("username", "donuts"),
-	)
-
-	var body []byte
-
-	tr := global.Tracer("example/client")
-	err := tr.WithSpan(ctx, "say hello",
-		func(ctx context.Context) error {
-			req, _ := http.NewRequestWithContext(ctx, "GET", *url, nil)
-
-			fmt.Printf("Sending request...\n")
-			res, err := client.Do(req)
-			if err != nil {
-				panic(err)
-			}
-			body, err = ioutil.ReadAll(res.Body)
-			_ = res.Body.Close()
-
-			return err
-		},
-		trace.WithAttributes(standard.PeerServiceKey.String("ExampleService")))
-
+	req, err := http.NewRequest("GET", *url, nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
+	fmt.Printf("sending request...\n")
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = res.Body.Close()
+
 	fmt.Printf("Response Received: %s\n\n\n", body)
-	fmt.Printf("Waiting for few seconds to export spans ...\n\n")
-	time.Sleep(10 * time.Second)
-	fmt.Printf("Inspect traces on stdout\n")
 }
--- instrumentation/github.com/astaxie/beego/example/go.mod
+++ instrumentation/github.com/astaxie/beego/example/go.mod
@@ -5,7 +5,6 @@ go 1.14
 require (
 	github.com/astaxie/beego v1.12.2
 	go.opentelemetry.io/contrib/instrumentation/github.com/astaxie/beego v0.0.0-00010101000000-000000000000
-	go.opentelemetry.io/contrib/instrumentation/net/http v0.10.1
 	go.opentelemetry.io/otel v0.10.0
 	go.opentelemetry.io/otel/exporters/stdout v0.10.0
 	go.opentelemetry.io/otel/sdk v0.10.0
