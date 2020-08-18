ac2b37f4f436421224c622e1887a1e8889859a98
Connor Lindsey <cblindsey3@gmail.com>
2020-08-18 11:41:35 -0600

    Make precommit

--- exporters/metric/cortex/cortex.go
+++ exporters/metric/cortex/cortex.go
@@ -129,7 +129,7 @@ func (e *Exporter) ConvertToTimeSeries(checkpointSet export.CheckpointSet) ([]*p
 				return err
 			}
 			timeSeries = append(timeSeries, tSeries...)
-		// Check if aggregation has sum value
+			// Check if aggregation has sum value
 		} else if sum, ok := agg.(aggregation.Sum); ok {
 			tSeries, err := convertFromSum(record, sum)
 			if err != nil {
@@ -157,7 +157,7 @@ func (e *Exporter) ConvertToTimeSeries(checkpointSet export.CheckpointSet) ([]*p
 					timeSeries = append(timeSeries, tSeries...)
 				}
 			}
-		// Check if aggregation has lastValue
+			// Check if aggregation has lastValue
 		} else if lastValue, ok := agg.(aggregation.LastValue); ok {
 			tSeries, err := convertFromLastValue(record, lastValue)
 			if err != nil {
