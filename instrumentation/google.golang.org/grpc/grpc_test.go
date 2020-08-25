daa9e0ca504c5f9c6dbacd1c02e7ce835a28b1fd
Tyler Yahn <codingalias@gmail.com>
2020-08-25 13:01:25 -0700

    Add testing for streaming gRPC

--- instrumentation/google.golang.org/grpc/grpc_test.go
+++ instrumentation/google.golang.org/grpc/grpc_test.go
@@ -21,23 +21,21 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/interop"
+	pb "google.golang.org/grpc/interop/grpc_testing"
+	"google.golang.org/grpc/test/bufconn"
+
 	otelgrpc "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc"
 	"go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/api/trace/tracetest"
 	"go.opentelemetry.io/otel/label"
 	"go.opentelemetry.io/otel/semconv"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/interop"
-	pb "google.golang.org/grpc/interop/grpc_testing"
-	"google.golang.org/grpc/test/bufconn"
 )
 
-const (
-	bufSize  = 2048
-	instName = "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc"
-)
+const bufSize = 2048
 
-func testUnaryCall(t *testing.T, cOpt []grpc.DialOption, sOpt []grpc.ServerOption) {
+func doCalls(cOpt []grpc.DialOption, sOpt []grpc.ServerOption) error {
 	l := bufconn.Listen(bufSize)
 	defer l.Close()
 
@@ -61,42 +59,62 @@ func testUnaryCall(t *testing.T, cOpt []grpc.DialOption, sOpt []grpc.ServerOptio
 		}, cOpt...)...,
 	)
 	if err != nil {
-		t.Fatalf("Failed to dial bufnet: %v", err)
+		return err
 	}
 	defer conn.Close()
 	client := pb.NewTestServiceClient(conn)
 
 	interop.DoEmptyUnaryCall(client)
 	interop.DoLargeUnaryCall(client)
+	interop.DoClientStreaming(client)
+	interop.DoServerStreaming(client)
+	interop.DoPingPong(client)
+
+	return nil
 }
 
-func TestUnaryInterceptors(t *testing.T) {
-	clientSR := new(tracetest.StandardSpanRecorder)
-	clientTracer := tracetest.NewProvider(tracetest.WithSpanRecorder(clientSR)).Tracer("TestUnaryClientInterceptor")
+func TestInterceptors(t *testing.T) {
+	clientUnarySR := new(tracetest.StandardSpanRecorder)
+	clientUnaryTracer := tracetest.NewProvider(tracetest.WithSpanRecorder(clientUnarySR)).Tracer("TestUnaryClientInterceptor")
 
-	serverSR := new(tracetest.StandardSpanRecorder)
-	serverTracer := tracetest.NewProvider(tracetest.WithSpanRecorder(serverSR)).Tracer("TestUnaryServerInterceptor")
+	clientStreamSR := new(tracetest.StandardSpanRecorder)
+	clientStreamTracer := tracetest.NewProvider(tracetest.WithSpanRecorder(clientStreamSR)).Tracer("TestStreamClientInterceptor")
 
-	testUnaryCall(
-		t,
+	serverUnarySR := new(tracetest.StandardSpanRecorder)
+	serverUnaryTracer := tracetest.NewProvider(tracetest.WithSpanRecorder(serverUnarySR)).Tracer("TestUnaryServerInterceptor")
+
+	serverStreamSR := new(tracetest.StandardSpanRecorder)
+	serverStreamTracer := tracetest.NewProvider(tracetest.WithSpanRecorder(serverStreamSR)).Tracer("TestStreamServerInterceptor")
+
+	assert.NoError(t, doCalls(
 		[]grpc.DialOption{
-			grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor(clientTracer)),
+			grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor(clientUnaryTracer)),
+			grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor(clientStreamTracer)),
 		},
 		[]grpc.ServerOption{
-			grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor(serverTracer)),
+			grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor(serverUnaryTracer)),
+			grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor(serverStreamTracer)),
 		},
-	)
+	))
 
 	t.Run("UnaryClientSpans", func(t *testing.T) {
-		checkClientSpans(t, clientTracer, clientSR.Completed())
+		checkUnaryClientSpans(t, clientUnaryTracer, clientUnarySR.Completed())
+	})
+
+	t.Run("StreamClientSpans", func(t *testing.T) {
+		checkStreamClientSpans(t, clientStreamTracer, clientStreamSR.Completed())
 	})
 
 	t.Run("UnaryServerSpans", func(t *testing.T) {
-		checkServerSpans(t, serverTracer, serverSR.Completed())
+		checkUnaryServerSpans(t, serverUnaryTracer, serverUnarySR.Completed())
+	})
+
+	t.Run("StreamServerSpans", func(t *testing.T) {
+		checkStreamServerSpans(t, serverStreamTracer, serverStreamSR.Completed())
 	})
 }
 
-func checkClientSpans(t *testing.T, tracer trace.Tracer, spans []*tracetest.Span) {
+func checkUnaryClientSpans(t *testing.T, tracer trace.Tracer, spans []*tracetest.Span) {
 	require.Len(t, spans, 2)
 
 	emptySpan := spans[0]
@@ -158,7 +176,372 @@ func checkClientSpans(t *testing.T, tracer trace.Tracer, spans []*tracetest.Span
 	}, largeSpan.Attributes())
 }
 
-func checkServerSpans(t *testing.T, tracer trace.Tracer, spans []*tracetest.Span) {
+func checkStreamClientSpans(t *testing.T, tracer trace.Tracer, spans []*tracetest.Span) {
+	require.Len(t, spans, 3)
+
+	streamInput := spans[0]
+	assert.True(t, streamInput.Ended())
+	assert.Equal(t, tracer, streamInput.Tracer())
+	assert.Equal(t, "grpc.testing.TestService/StreamingInputCall", streamInput.Name())
+	// sizes from reqSizes in "google.golang.org/grpc/interop".
+	assert.Equal(t, []tracetest.Event{
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(27190),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(12),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(1834),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(45912),
+			},
+		},
+		// client does not record an event for the server response.
+	}, noTimestamp(streamInput.Events()))
+	assert.Equal(t, map[label.Key]label.Value{
+		semconv.RPCMethodKey:      label.StringValue("StreamingInputCall"),
+		semconv.RPCServiceKey:     label.StringValue("grpc.testing.TestService"),
+		semconv.RPCSystemGRPC.Key: semconv.RPCSystemGRPC.Value,
+	}, streamInput.Attributes())
+
+	streamOutput := spans[1]
+	assert.True(t, streamOutput.Ended())
+	assert.Equal(t, tracer, streamOutput.Tracer())
+	assert.Equal(t, "grpc.testing.TestService/StreamingOutputCall", streamOutput.Name())
+	// sizes from respSizes in "google.golang.org/grpc/interop".
+	assert.Equal(t, []tracetest.Event{
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(21),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(31423),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(13),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(2659),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(58987),
+			},
+		},
+	}, noTimestamp(streamOutput.Events()))
+	assert.Equal(t, map[label.Key]label.Value{
+		semconv.RPCMethodKey:      label.StringValue("StreamingOutputCall"),
+		semconv.RPCServiceKey:     label.StringValue("grpc.testing.TestService"),
+		semconv.RPCSystemGRPC.Key: semconv.RPCSystemGRPC.Value,
+	}, streamOutput.Attributes())
+
+	pingPong := spans[2]
+	assert.True(t, pingPong.Ended())
+	assert.Equal(t, tracer, pingPong.Tracer())
+	assert.Equal(t, "grpc.testing.TestService/FullDuplexCall", pingPong.Name())
+	assert.Equal(t, []tracetest.Event{
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(27196),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(31423),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(16),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(13),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(1839),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(2659),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(45918),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(58987),
+			},
+		},
+	}, noTimestamp(pingPong.Events()))
+	assert.Equal(t, map[label.Key]label.Value{
+		semconv.RPCMethodKey:      label.StringValue("FullDuplexCall"),
+		semconv.RPCServiceKey:     label.StringValue("grpc.testing.TestService"),
+		semconv.RPCSystemGRPC.Key: semconv.RPCSystemGRPC.Value,
+	}, pingPong.Attributes())
+}
+
+func checkStreamServerSpans(t *testing.T, tracer trace.Tracer, spans []*tracetest.Span) {
+	require.Len(t, spans, 3)
+
+	streamInput := spans[0]
+	assert.True(t, streamInput.Ended())
+	assert.Equal(t, tracer, streamInput.Tracer())
+	assert.Equal(t, "grpc.testing.TestService/StreamingInputCall", streamInput.Name())
+	// sizes from reqSizes in "google.golang.org/grpc/interop".
+	assert.Equal(t, []tracetest.Event{
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(27190),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(12),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(1834),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(45912),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(4),
+			},
+		},
+	}, noTimestamp(streamInput.Events()))
+	assert.Equal(t, map[label.Key]label.Value{
+		semconv.RPCMethodKey:      label.StringValue("StreamingInputCall"),
+		semconv.RPCServiceKey:     label.StringValue("grpc.testing.TestService"),
+		semconv.RPCSystemGRPC.Key: semconv.RPCSystemGRPC.Value,
+	}, streamInput.Attributes())
+
+	streamOutput := spans[1]
+	assert.True(t, streamOutput.Ended())
+	assert.Equal(t, tracer, streamOutput.Tracer())
+	assert.Equal(t, "grpc.testing.TestService/StreamingOutputCall", streamOutput.Name())
+	// sizes from respSizes in "google.golang.org/grpc/interop".
+	assert.Equal(t, []tracetest.Event{
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(21),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(31423),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(13),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(2659),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(58987),
+			},
+		},
+	}, noTimestamp(streamOutput.Events()))
+	assert.Equal(t, map[label.Key]label.Value{
+		semconv.RPCMethodKey:      label.StringValue("StreamingOutputCall"),
+		semconv.RPCServiceKey:     label.StringValue("grpc.testing.TestService"),
+		semconv.RPCSystemGRPC.Key: semconv.RPCSystemGRPC.Value,
+	}, streamOutput.Attributes())
+
+	pingPong := spans[2]
+	assert.True(t, pingPong.Ended())
+	assert.Equal(t, tracer, pingPong.Tracer())
+	assert.Equal(t, "grpc.testing.TestService/FullDuplexCall", pingPong.Name())
+	assert.Equal(t, []tracetest.Event{
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(27196),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(1),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(31423),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(16),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(2),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(13),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(1839),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(3),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(2659),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("RECEIVED"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(45918),
+			},
+		},
+		{
+			Name: "message",
+			Attributes: map[label.Key]label.Value{
+				semconv.RPCMessageIDKey:               label.IntValue(4),
+				semconv.RPCMessageTypeKey:             label.StringValue("SENT"),
+				semconv.RPCMessageUncompressedSizeKey: label.IntValue(58987),
+			},
+		},
+	}, noTimestamp(pingPong.Events()))
+	assert.Equal(t, map[label.Key]label.Value{
+		semconv.RPCMethodKey:      label.StringValue("FullDuplexCall"),
+		semconv.RPCServiceKey:     label.StringValue("grpc.testing.TestService"),
+		semconv.RPCSystemGRPC.Key: semconv.RPCSystemGRPC.Value,
+	}, pingPong.Attributes())
+}
+
+func checkUnaryServerSpans(t *testing.T, tracer trace.Tracer, spans []*tracetest.Span) {
 	require.Len(t, spans, 2)
 
 	emptySpan := spans[0]
