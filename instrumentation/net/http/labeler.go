7183b63a130e9c1609a7cea95953b088a181be17
Anthony J Mirabella <a9@aneurysm9.com>
2020-08-28 17:47:05 -0400

    Address PR Feedback:
    * Add mutex around access to labels
    * Add Labeler use to handler example test

--- instrumentation/net/http/labeler.go
+++ instrumentation/net/http/labeler.go
@@ -16,6 +16,7 @@ package http
 
 import (
 	"context"
+	"sync"
 
 	"go.opentelemetry.io/otel/label"
 )
@@ -23,11 +24,14 @@ import (
 // Labeler is used to allow instrumented HTTP handlers to add custom labels to
 // the metrics recorded by the net/http instrumentation.
 type Labeler struct {
+	mu     sync.Mutex
 	labels []label.KeyValue
 }
 
 // Add labels to a Labeler.
 func (l *Labeler) Add(ls ...label.KeyValue) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
 	l.labels = append(l.labels, ls...)
 }
 
@@ -40,7 +44,7 @@ func injectLabeler(ctx context.Context, l *Labeler) context.Context {
 }
 
 // LabelerFromContext retrieves a Labeler instance from the provided context if
-// one is available.  If no labeler was found in the provided context a new, empty
+// one is available.  If no Labeler was found in the provided context a new, empty
 // Labeler is returned and the second return value is false.  In this case it is
 // safe to use the Labeler but any labels added to it will not be used.
 func LabelerFromContext(ctx context.Context) (*Labeler, bool) {
