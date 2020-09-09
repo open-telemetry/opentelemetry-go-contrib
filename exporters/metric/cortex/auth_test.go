3ea5332df8aac7c3f4804388aee19da97bce02ae
Eric Lee <elleea@amazon.com>
2020-09-09 11:14:38 -0700

    Restore tests for auth_test.go on 386 (#334)

--- exporters/metric/cortex/auth_test.go
+++ exporters/metric/cortex/auth_test.go
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Remove when #310 is resolved.
-// +build !386
-
 package cortex
 
 import (
