99fbe980443fb961bd7cffab9eaaec7448ab34ed
Tenaria <30540604+Tenaria@users.noreply.github.com>
2020-10-20 10:47:14 +1100

    Update instrumentation/github.com/aws/aws-sdk-go/service/otels3/s3.go
    Co-authored-by: Zhihao Chen <chenzhihao@users.noreply.github.com>

--- instrumentation/github.com/aws/aws-sdk-go/service/otels3/s3.go
+++ instrumentation/github.com/aws/aws-sdk-go/service/otels3/s3.go
@@ -173,7 +173,7 @@ func createRecorders(meter metric.Meter) *recorders {
 
 func NewInstrumentedS3Client(s s3iface.S3API, opts ...config.Option) (s3iface.S3API, error) {
 	if s == nil || reflect.ValueOf(s).IsNil() {
-		return &instrumentedS3{}, fmt.Errorf("interface must be set")
+		return &instrumentedS3{}, errors.New("interface must be set")
 	}
 
 	cfg := config.Config{}
