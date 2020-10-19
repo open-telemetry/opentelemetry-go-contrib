6bc33a36e7cb3a9e0c5c87cc0bb85ce9ce83c28e
Claire Fei <cfei@atlassian.com>
2020-10-19 13:03:02 +1100

    generalises other section in s3 tests that relices on hardcoded indices

--- instrumentation/github.com/aws/aws-sdk-go/service/otels3/s3_test.go
+++ instrumentation/github.com/aws/aws-sdk-go/service/otels3/s3_test.go
@@ -2,6 +2,7 @@ package otels3
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -133,8 +134,29 @@ func Test_instrumentedS3_PutObjectWithContext(t *testing.T) {
 
 			// In Meter we have one duration recorder, one operation counter
 			assert.Equal(t, 2, len(mockedMeterImp.MeasurementBatches))
-			assert.Equal(t, "storage.operation.duration_μs", mockedMeterImp.MeasurementBatches[0].Measurements[0].Instrument.Descriptor().Name())
-			assert.Equal(t, "storage.s3.operation", mockedMeterImp.MeasurementBatches[1].Measurements[0].Instrument.Descriptor().Name())
+
+			metricsFound := map[string]bool{
+				"storage.operation.duration_μs": false,
+				"storage.s3.operation":          false,
+			}
+			// iterate over metrics to get names
+			for _, measurementBatch := range mockedMeterImp.MeasurementBatches {
+				for _, measurement := range measurementBatch.Measurements {
+					metricName := measurement.Instrument.Descriptor().Name()
+					//check if we are looking for this metric name, if so, mark as found
+					if _, ok := metricsFound[metricName]; ok {
+						metricsFound[metricName] = true
+					}
+				}
+			}
+
+			//check all metric names are found
+			for metricName, metricFound := range metricsFound {
+				if metricFound == false {
+					err := fmt.Sprintf("metric not found: %s", metricName)
+					panic(err)
+				}
+			}
 
 			for _, measurementBatch := range mockedMeterImp.MeasurementBatches {
 				if tt.fields.spanCorrelationInMetrics {
