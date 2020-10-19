76ec70079c53da4fc8d4c96ce4389c829f2438d7
Zhihao Chen <zchen2@atlassian.com>
2020-10-19 11:13:26 +1100

    update test

--- instrumentation/github.com/aws/aws-sdk-go/service/otels3/s3_test.go
+++ instrumentation/github.com/aws/aws-sdk-go/service/otels3/s3_test.go
@@ -2,28 +2,45 @@ package otels3
 
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go/service/otels3/mocks"
+	mockmetric "go.opentelemetry.io/contrib/internal/metric"
+	mocktrace "go.opentelemetry.io/contrib/internal/trace"
+	"go.opentelemetry.io/otel/label"
+
+	"reflect"
+	"testing"
+
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/metric"
 	"go.opentelemetry.io/otel/api/trace"
-	"reflect"
-	"testing"
 )
 
 var (
-	mockedMeter = global.MeterProvider().Meter(
-		"github.com/aws/aws-sdk-go/aws/service/s3",
-	)
-	mockedTracer      = mocks.NewTracerProvider().Tracer("github.com/aws/aws-sdk-go/aws/service/s3")
-	mockedCounters    = createCounters(mockedMeter)
-	mockedRecorders   = createRecorders(mockedMeter)
-	mockedPropagators = global.TextMapPropagator()
+	s3bucket = "s3bucket"
 )
+
+func getLabelValFromMeasurementBatch(key label.Key, batch mockmetric.Batch) *label.Value {
+	for _, label := range batch.Labels {
+		if label.Key == key {
+			return &label.Value
+		}
+	}
+	return nil
+}
+
+func getLabelValFromSpan(key label.Key, span mocktrace.Span) *label.Value {
+	if value, ok := span.Attributes[key]; ok {
+		return &value
+	}
+	return nil
+}
 
 func Test_instrumentedS3_PutObjectWithContext(t *testing.T) {
 	type fields struct {
@@ -31,7 +48,7 @@ func Test_instrumentedS3_PutObjectWithContext(t *testing.T) {
 		tracer                   trace.Tracer
 		meter                    metric.Meter
 		spanCorrelationInMetrics bool
-		mockSetup                func(s3Client *mock.Mock) interface{}
+		mockSetup                func(s3Client *mock.Mock) (expectedReturn interface{})
 	}
 	type args struct {
 		ctx   aws.Context
@@ -45,7 +62,27 @@ func Test_instrumentedS3_PutObjectWithContext(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "instrumentedS3.PutObjectWithContext should be delegated to S3.PutObjectWithContext",
+			name: "instrumentedS3.PutObjectWithContext should be delegated to S3.PutObjectWithContext while metrics and spans are linked",
+			fields: fields{
+				S3API:                    &mocks.S3Client{},
+				spanCorrelationInMetrics: true,
+				mockSetup: func(m *mock.Mock) (expectedReturn interface{}) {
+					expectedReturn = &s3.PutObjectOutput{}
+					m.On("PutObjectWithContext", mock.Anything, mock.Anything).Return(expectedReturn, nil)
+					return
+				},
+			},
+			args: args{
+				ctx: context.Background(),
+				input: &s3.PutObjectInput{
+					Bucket: &s3bucket,
+				},
+				opts: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "instrumentedS3.PutObjectWithContext should be delegated to S3.PutObjectWithContext while metrics and spans are NOT linked",
 			fields: fields{
 				S3API:                    &mocks.S3Client{},
 				spanCorrelationInMetrics: false,
@@ -56,15 +93,25 @@ func Test_instrumentedS3_PutObjectWithContext(t *testing.T) {
 				},
 			},
 			args: args{
-				ctx:   context.Background(),
-				input: &s3.PutObjectInput{},
-				opts:  nil,
+				ctx: context.Background(),
+				input: &s3.PutObjectInput{
+					Bucket: &s3bucket,
+				},
+				opts: nil,
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			_, mockedTracer := mocktrace.NewTracerProviderAndTracer("github.com/aws/aws-sdk-go/aws/service/s3")
+			mockedMeterImp, mockedMeter := mockmetric.NewMeter()
+			mockedCounters := createCounters(mockedMeter)
+			mockedRecorders := createRecorders(mockedMeter)
+			mockedPropagators := global.TextMapPropagator()
+
 			s3Mock := &mocks.S3Client{}
 			s := &instrumentedS3{
 				S3API:                    s3Mock,
@@ -83,6 +130,34 @@ func Test_instrumentedS3_PutObjectWithContext(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, expectedReturn) {
 				t.Errorf("PutObjectWithContext() got = %v, want %v", got, expectedReturn)
+			}
+			spans := mockedTracer.EndedSpans()
+			assert.Equal(t, 1, len(spans))
+			assert.Equal(t, trace.SpanKindClient, spans[0].Kind)
+			assert.Equal(t, s3StorageSystemValue, getLabelValFromSpan(storageSystemKey, *spans[0]).AsString())
+			assert.Equal(t, *tt.args.input.Bucket, getLabelValFromSpan(storageDestinationKey, *spans[0]).AsString())
+			assert.Equal(t, operationPutObject, getLabelValFromSpan(storageOperationKey, *spans[0]).AsString())
+
+			// In Meter we have one duration recorder, one operation counter
+			assert.Equal(t, 2, len(mockedMeterImp.MeasurementBatches))
+			assert.Equal(t, "storage.operation.duration_μs", mockedMeterImp.MeasurementBatches[0].Measurements[0].Instrument.Descriptor().Name())
+			assert.Equal(t, "storage.s3.operation", mockedMeterImp.MeasurementBatches[1].Measurements[0].Instrument.Descriptor().Name())
+
+			if tt.fields.spanCorrelationInMetrics {
+				traceID := spans[0].SpanContext().TraceID.String()
+				spanID := spans[0].SpanContext().SpanID.String()
+
+				assert.Equal(t, traceID, getLabelValFromMeasurementBatch("trace.id", mockedMeterImp.MeasurementBatches[0]).AsString())
+				assert.Equal(t, spanID, getLabelValFromMeasurementBatch("span.id", mockedMeterImp.MeasurementBatches[0]).AsString())
+
+				assert.Equal(t, traceID, getLabelValFromMeasurementBatch("trace.id", mockedMeterImp.MeasurementBatches[1]).AsString())
+				assert.Equal(t, spanID, getLabelValFromMeasurementBatch("span.id", mockedMeterImp.MeasurementBatches[1]).AsString())
+			} else {
+				assert.Nil(t, getLabelValFromMeasurementBatch("trace.id", mockedMeterImp.MeasurementBatches[0]))
+				assert.Nil(t, getLabelValFromMeasurementBatch("span.id", mockedMeterImp.MeasurementBatches[0]))
+
+				assert.Nil(t, getLabelValFromMeasurementBatch("trace.id", mockedMeterImp.MeasurementBatches[1]))
+				assert.Nil(t, getLabelValFromMeasurementBatch("span.id", mockedMeterImp.MeasurementBatches[1]))
 			}
 		})
 	}
