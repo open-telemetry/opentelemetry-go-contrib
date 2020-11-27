9d5e8334c39d9cc04533ccb75e4b8845b8893dca
Wilbert Guo <wilbeguo@amazon.com>
2020-11-27 14:08:07 -0800

    Add check for HTTP response status code in error handling

--- detectors/aws/eks/eks_resource_detector.go
+++ detectors/aws/eks/eks_resource_detector.go
@@ -174,8 +174,8 @@ func (eksUtils eksDetectorUtils) fetchString(httpMethod string, URL string) (str
 
 	// Execute HTTP request
 	response, err := client.Do(request)
-	if err != nil {
-		return "", fmt.Errorf("failed to execute HTTP request with method=%s, URL=%s: %w", httpMethod, URL, err)
+	if err != nil || response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to execute HTTP request with method=%s, URL=%s, Status Code=%d: %w", httpMethod, URL, response.StatusCode, err)
 	}
 
 	// Retrieve response body from HTTP request
