3981323d836411cd1171ba0a51ebb1fa6795b7ac
gokhan.karadas <gokhan.karadas1992@gmail.com>
2021-01-11 09:30:35 +0300

    doc file

--- instrumentation/github.com/streadway/amqp/otelamqp/doc.go
+++ instrumentation/github.com/streadway/amqp/otelamqp/doc.go
@@ -12,22 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package otelamqp
-
-// amqpHeadersCarrier satisfies with TextMapPropagator
-type amqpHeadersCarrier map[string]interface{}
-
-// Get returns the value associated with the passed key.
-func (c amqpHeadersCarrier) Get(key string) string {
-	for k, v := range c {
-		if k == key {
-			return v.(string)
-		}
-	}
-	return ""
-}
-
-// Set stores the key-value pair.
-func (c amqpHeadersCarrier) Set(key, val string) {
-	c[key] = val
-}
+// Package amqp instruments the github.com/streadway/amqp.
+package otelamqp // import "go.opentelemetry.io/contrib/instrumentation/github.com/streadway/amqp/otelamqp"
