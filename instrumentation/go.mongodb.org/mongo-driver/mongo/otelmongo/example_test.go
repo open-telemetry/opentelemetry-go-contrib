6d3a16345f519e9ecf5d971ce6d8737cb70deb40
ET <evantorrie@users.noreply.github.com>
2021-05-03 10:54:08 -0700

    Remove service.name attribute (#763)
    * Remove service.name attribute
    
    `service.name` should correspond to the Resource of the entity
    producing telemetry, not the remote service. If anything, the
    attribute that should be used for a database service is `peer.service`.
    
    * remember to make precommit first
    
    * Add note to CHANGELOG regarding removal of service.name attribute

--- instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo/example_test.go
+++ instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo/example_test.go
@@ -27,7 +27,7 @@ import (
 func Example() {
 	// connect to MongoDB
 	opts := options.Client()
-	opts.Monitor = otelmongo.NewMonitor("test-service")
+	opts.Monitor = otelmongo.NewMonitor()
 	opts.ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.Background(), opts)
 	if err != nil {
