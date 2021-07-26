d481dcb5b0ceca555c3a3550d325ef33f1709fae
Garrett Egan <22334769+garrettwegan@users.noreply.github.com>
2021-07-26 12:37:16 -0700

    Add example

---
+++ instrumentation/github.com/aws/aws-lambda-go/otellambda/example/go.mod
@@ -0,0 +1,22 @@
+module go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-g/otellambda/example
+
+go 1.16
+
+replace (
+	go.opentelemetry.io/contrib/detectors/aws/lambda => ../../../../../../detectors/aws/lambda
+	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda => ../
+)
+
+require (
+	github.com/aws/aws-lambda-go v1.25.0
+	github.com/aws/aws-sdk-go-v2/config v1.5.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.11.1
+	go.opentelemetry.io/contrib/detectors/aws/lambda v0.21.0
+	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda v0.21.0
+	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go-v2/otelaws v0.21.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.21.0
+	go.opentelemetry.io/otel v1.0.0-RC1
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.0.0-RC1
+	go.opentelemetry.io/otel/sdk v1.0.0-RC1
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ instrumentation/github.com/aws/aws-lambda-go/otellambda/example/manualAWSCleanup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# constants
+LAMBDA_FUNCTION_NAME=SampleLambdaGo
+ROLE_NAME="$LAMBDA_FUNCTION_NAME"Role
+POLICY_NAME="$LAMBDA_FUNCTION_NAME"Policy
+LOG_GROUP_NAME=/aws/lambda/"$LAMBDA_FUNCTION_NAME"
+AWS_ACCT_ID=$(aws sts get-caller-identity | jq '.Account | tonumber')
+ERROR_LOG_FILE=manualAWSCleanupErrors.log
+
+# Clear log
+rm $ERROR_LOG_FILE 2> /dev/null
+
+# clear log group of all streams
+if aws logs describe-log-streams --log-group-name "$LOG_GROUP_NAME" > /dev/null 2>> $ERROR_LOG_FILE ; then
+  LOG_STREAM_NAME=$(aws logs describe-log-streams --log-group-name "$LOG_GROUP_NAME" --order-by LastEventTime --descending | jq --raw-output '.logStreams[0].logStreamName')
+  while [ "$LOG_STREAM_NAME" != "null" ] ; do
+    aws logs delete-log-stream --log-group-name "$LOG_GROUP_NAME" --log-stream-name "$LOG_STREAM_NAME" 2>> $ERROR_LOG_FILE && echo "Deleted log stream $LOG_STREAM_NAME"
+    LOG_STREAM_NAME=$(aws logs describe-log-streams --log-group-name "$LOG_GROUP_NAME" --order-by LastEventTime --descending | jq --raw-output '.logStreams[0].logStreamName')
+  done
+  aws logs delete-log-group --log-group-name "$LOG_GROUP_NAME" && echo "Deleted log group $LOG_GROUP_NAME"
+else
+  echo "Did not delete log group, likely already deleted"
+fi
+
+# destroy remaining lambda resources if they exist
+aws lambda delete-function --function-name "$LAMBDA_FUNCTION_NAME" 2>> $ERROR_LOG_FILE && echo "Deleted Lambda Function $LAMBDA_FUNCTION_NAME" || echo "Did not delete function, likely already deleted"
+aws iam detach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::"$AWS_ACCT_ID":policy/"$POLICY_NAME" 2>> $ERROR_LOG_FILE && echo "Detached $POLICY_NAME from $ROLE_NAME" || echo "Did not detach policy from role, likely already detached"
+aws iam delete-policy --policy-arn arn:aws:iam::"$AWS_ACCT_ID":policy/"$POLICY_NAME" 2>> $ERROR_LOG_FILE && echo "Deleted IAM Policy POLICY_NAME" || echo "Did not delete IAM Policy, likely already deleted"
+aws iam delete-role --role-name "$ROLE_NAME" 2>> $ERROR_LOG_FILE && echo "Deleted IAM Role $ROLE_NAME" || echo "Did not delete IAM Role, likely already deleted"
+
+if [ -s $ERROR_LOG_FILE ] ; then
+  echo 'Some resources failed to delete. Can ensure these errors were due to the resources existing by checking "'$ERROR_LOG_FILE'"'
+fi
