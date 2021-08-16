13f6e4e957e834bb3adfcd8b65bd159e5329e410
Tyler Yahn <codingalias@gmail.com>
2021-08-16 15:19:10 -0700

    Use Noop TracerProvider instead of oteltest

--- instrumentation/github.com/astaxie/beego/otelbeego/beego_test.go
+++ instrumentation/github.com/astaxie/beego/otelbeego/beego_test.go
@@ -38,6 +38,7 @@ import (
 
 	"github.com/astaxie/beego"
 	beegoCtx "github.com/astaxie/beego/context"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -185,20 +186,40 @@ func TestWithFilters(t *testing.T) {
 	}
 }
 
+func ctxTest() (context.Context, func(*testing.T, context.Context)) {
+	ctx := context.Background()
+	sc := trace.NewSpanContext(trace.SpanContextConfig{
+		TraceID: trace.TraceID{0x01},
+		SpanID:  trace.SpanID{0x01},
+		Remote:  true,
+	})
+	ctx = trace.ContextWithSpanContext(ctx, sc)
+
+	return ctx, func(t *testing.T, ctx context.Context) {
+		got := trace.SpanContextFromContext(ctx)
+		assert.Equal(t, sc.TraceID(), got.TraceID())
+		assert.Equal(t, sc.SpanID(), got.SpanID())
+		assert.Equal(t, sc.TraceFlags(), got.TraceFlags())
+		assert.Equal(t, sc.TraceState(), got.TraceState())
+		assert.Equal(t, sc.IsRemote(), got.IsRemote())
+	}
+}
+
 func TestSpanFromContextDefaultProvider(t *testing.T) {
 	defer replaceBeego()
 	_, provider := metrictest.NewMeterProvider()
 	global.SetMeterProvider(provider)
-	otel.SetTracerProvider(oteltest.NewTracerProvider())
+	otel.SetTracerProvider(trace.NewNoopTracerProvider())
 
+	ctx, eval := ctxTest()
 	router := beego.NewControllerRegister()
 	router.Get("/hello-with-span", func(ctx *beegoCtx.Context) {
-		assertSpanFromContext(ctx.Request.Context(), t)
+		eval(t, ctx.Request.Context())
 		ctx.ResponseWriter.WriteHeader(http.StatusAccepted)
 	})
 
 	rr := httptest.NewRecorder()
-	req, err := http.NewRequest(http.MethodGet, "http://localhost/hello-with-span", nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost/hello-with-span", nil)
 	require.NoError(t, err)
 
 	mw := NewOTelBeegoMiddleWare(middleWareName)
@@ -211,19 +232,20 @@ func TestSpanFromContextDefaultProvider(t *testing.T) {
 func TestSpanFromContextCustomProvider(t *testing.T) {
 	defer replaceBeego()
 	_, provider := metrictest.NewMeterProvider()
+	ctx, eval := ctxTest()
 	router := beego.NewControllerRegister()
 	router.Get("/hello-with-span", func(ctx *beegoCtx.Context) {
-		assertSpanFromContext(ctx.Request.Context(), t)
+		eval(t, ctx.Request.Context())
 		ctx.ResponseWriter.WriteHeader(http.StatusAccepted)
 	})
 
 	rr := httptest.NewRecorder()
-	req, err := http.NewRequest(http.MethodGet, "http://localhost/hello-with-span", nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost/hello-with-span", nil)
 	require.NoError(t, err)
 
 	mw := NewOTelBeegoMiddleWare(
 		middleWareName,
-		WithTracerProvider(oteltest.NewTracerProvider()),
+		WithTracerProvider(trace.NewNoopTracerProvider()),
 		WithMeterProvider(provider),
 	)
 
@@ -399,16 +421,6 @@ func assertMetrics(t *testing.T, batches []metrictest.Batch, tc *testCase) {
 			require.Contains(t, batch.Labels, att)
 		}
 	}
-}
-
-func assertSpanFromContext(ctx context.Context, t *testing.T) {
-	span := trace.SpanFromContext(ctx)
-	_, ok := span.(*oteltest.Span)
-	require.True(t, ok)
-	spanTracer := span.TracerProvider().Tracer("go.opentelemetry.io/contrib/instrumentation/github/astaxie/beego/otelbeego")
-	mockTracer, ok := spanTracer.(*oteltest.Tracer)
-	require.True(t, ok)
-	require.NotEmpty(t, mockTracer.Name)
 }
 
 // ------------------------------------------ Test Cases
