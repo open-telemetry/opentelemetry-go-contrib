1c083fd4e17a5007d320b7e0c150959df80ff57f
Punya Biswal <punya@google.com>
2022-03-15 11:42:24 -0400

    Demonstrate a nested span inside the RPC server (#1893)
    * Demonstrate a nested span inside the RPC server
    
    * Remove ineffectual assignment
    
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- instrumentation/google.golang.org/grpc/otelgrpc/example/server/main.go
+++ instrumentation/google.golang.org/grpc/otelgrpc/example/server/main.go
@@ -22,6 +22,10 @@ import (
 	"net"
 	"time"
 
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc/example/api"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc/example/config"
 
@@ -34,6 +38,8 @@ const (
 	port = ":7777"
 )
 
+var tracer = otel.Tracer("grpc-example")
+
 // server is used to implement api.HelloServiceServer
 type server struct {
 	api.HelloServiceServer
@@ -42,9 +48,18 @@ type server struct {
 // SayHello implements api.HelloServiceServer
 func (s *server) SayHello(ctx context.Context, in *api.HelloRequest) (*api.HelloResponse, error) {
 	log.Printf("Received: %v\n", in.GetGreeting())
+	s.workHard(ctx)
 	time.Sleep(50 * time.Millisecond)
 
 	return &api.HelloResponse{Reply: "Hello " + in.Greeting}, nil
+}
+
+func (s *server) workHard(ctx context.Context) {
+	_, span := tracer.Start(ctx, "workHard",
+		trace.WithAttributes(attribute.String("extra.key", "extra.value")))
+	defer span.End()
+
+	time.Sleep(50 * time.Millisecond)
 }
 
 func (s *server) SayHelloServerStream(in *api.HelloRequest, out api.HelloService_SayHelloServerStreamServer) error {
