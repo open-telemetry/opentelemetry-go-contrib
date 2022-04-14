19fe3392c93f0f2323ff3eb5b841115ccfbc83dc
drewtoto <drewtoto@users.noreply.github.com>
2022-04-14 10:00:21 -0700

    Apply suggestions from code review (#2166)
    - Add Copyright text
    - Formatting and spacing
    - Change MultimapCarrier methods receiver to pointer
    - Use MultimapCarrier pointer in compile time check
    - Update MultimapCarrier methods
    - Update assertion that MultimapCarrier implements TextMapCarrier
    
    Update `GrpcPropagationMiddleware` to use
    `MultimapCarrier` pointer type.

--- instrumentation/github.com/go-kit/kit/otelkit/propagation.go
+++ instrumentation/github.com/go-kit/kit/otelkit/propagation.go
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package otelkit
 
 import (
@@ -5,19 +19,20 @@ import (
 	"net/http"
 
 	"github.com/go-kit/kit/endpoint"
+	"google.golang.org/grpc/metadata"
+
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
-	"google.golang.org/grpc/metadata"
 )
 
 type MultimapCarrier map[string][]string
 
-// Compile time check that MapCarrier implements the TextMapCarrier.
-var _ propagation.TextMapCarrier = MultimapCarrier{}
+// Compile time check that MultimapCarrier implements the TextMapCarrier.
+var _ propagation.TextMapCarrier = &MultimapCarrier{}
 
 // Get returns the value associated with the passed key.
-func (c MultimapCarrier) Get(key string) string {
-	v := c[key]
+func (c *MultimapCarrier) Get(key string) string {
+	v := (*c)[key]
 	if len(v) > 0 {
 		return v[0]
 	}
@@ -25,14 +40,14 @@ func (c MultimapCarrier) Get(key string) string {
 }
 
 // Set stores the key-value pair.
-func (c MultimapCarrier) Set(key, value string) {
-	c[key] = []string{value}
+func (c *MultimapCarrier) Set(key, value string) {
+	(*c)[key] = []string{value}
 }
 
 // Keys lists the keys stored in this carrier.
-func (c MultimapCarrier) Keys() []string {
-	keys := make([]string, 0, len(c))
-	for k := range c {
+func (c *MultimapCarrier) Keys() []string {
+	keys := make([]string, 0, len(*c))
+	for k := range *c {
 		keys = append(keys, k)
 	}
 	return keys
@@ -45,7 +60,7 @@ func GrpcPropagationMiddleware() endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 			if md, ok := metadata.FromIncomingContext(ctx); ok {
-				ctx = otel.GetTextMapPropagator().Extract(ctx, MultimapCarrier(md))
+				ctx = otel.GetTextMapPropagator().Extract(ctx, (*MultimapCarrier)(&md))
 			}
 			return next(ctx, request)
 		}
