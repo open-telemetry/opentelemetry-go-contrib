da6ba5772e12dda94483079cf3183d032adc6d7b
drew <drewtoto@gmail.com>
2022-04-15 14:26:39 -0700

    Remove type duplicated by http.Header (#2166)
    Remove `MultimapCarrier` as it has the same underlying datatype as
    `HeaderCarrier`.

--- instrumentation/github.com/go-kit/kit/otelkit/propagation.go
+++ instrumentation/github.com/go-kit/kit/otelkit/propagation.go
@@ -25,34 +25,6 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 )
 
-type MultimapCarrier map[string][]string
-
-// Compile time check that MultimapCarrier implements the TextMapCarrier.
-var _ propagation.TextMapCarrier = &MultimapCarrier{}
-
-// Get returns the value associated with the passed key.
-func (c *MultimapCarrier) Get(key string) string {
-	v := (*c)[key]
-	if len(v) > 0 {
-		return v[0]
-	}
-	return ""
-}
-
-// Set stores the key-value pair.
-func (c *MultimapCarrier) Set(key, value string) {
-	(*c)[key] = []string{value}
-}
-
-// Keys lists the keys stored in this carrier.
-func (c *MultimapCarrier) Keys() []string {
-	keys := make([]string, 0, len(*c))
-	for k := range *c {
-		keys = append(keys, k)
-	}
-	return keys
-}
-
 // GrpcPropagationMiddleware uses gRPC metadata from the incoming context,
 // if it exists, and extracts the traceparent to propagate context information
 // that enables distributed tracing.
@@ -60,7 +32,7 @@ func GRPCPropagationMiddleware() endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 			if md, ok := metadata.FromIncomingContext(ctx); ok {
-				ctx = otel.GetTextMapPropagator().Extract(ctx, (*MultimapCarrier)(&md))
+				ctx = otel.GetTextMapPropagator().Extract(ctx, propagation.HeaderCarrier(md))
 			}
 			return next(ctx, request)
 		}
