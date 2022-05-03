1055a5c94ec7d79d5639e7ba67ece74103318e82
Tyler Yahn <codingalias@gmail.com>
2022-05-03 15:00:46 -0700

    Go import lint fix

--- propagators/autoprop/registry.go
+++ propagators/autoprop/registry.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package autoprop
+package autoprop // import "go.opentelemetry.io/contrib/propagators/autoprop"
 
 import (
 	"sync"
