756c3fdac95cdfaccd832970c751f55ffe7ec77f
Damien Mathieu <42@dmathieu.com>
2022-06-09 16:57:30 +0200

    note that WithPublicEndpoint takes precedence

--- instrumentation/net/http/otelhttp/config.go
+++ instrumentation/net/http/otelhttp/config.go
@@ -116,6 +116,7 @@ func WithPublicEndpoint() Option {
 // configuring the Handler to link the span with an incoming span context. If
 // this option is not provided or returns false, then the association is a
 // child association instead of a link.
+// Note: WithPublicEndpoint takes precedence over WithPublicEndpointFn.
 func WithPublicEndpointFn(fn func(*http.Request) bool) Option {
 	return optionFunc(func(c *config) {
 		c.PublicEndpointFn = fn
