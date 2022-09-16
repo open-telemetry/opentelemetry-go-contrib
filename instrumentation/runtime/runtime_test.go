1ea6dddfcf972a87f6efa13459732df0595e50dc
Tyler Yahn <codingalias@gmail.com>
2022-09-16 10:04:38 -0700

    Add PR number for TODO

--- instrumentation/runtime/runtime_test.go
+++ instrumentation/runtime/runtime_test.go
@@ -14,7 +14,7 @@
 
 package runtime_test
 
-// TODO(#TBD): Add integration tests for the runtime instrumentation. These
+// TODO(#2757): Add integration tests for the runtime instrumentation. These
 // tests depend on
 // https://github.com/open-telemetry/opentelemetry-go/issues/3031 being
 // resolved.
