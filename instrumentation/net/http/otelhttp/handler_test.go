52c663fdfb916948e18480c337772c03184a83e3
Damien Mathieu <42@dmathieu.com>
2022-12-15 17:30:54 +0100

    provide testing.T to the handler

--- instrumentation/net/http/otelhttp/handler_test.go
+++ instrumentation/net/http/otelhttp/handler_test.go
@@ -30,49 +30,57 @@ import (
 func TestHandler(t *testing.T) {
 	testCases := []struct {
 		name               string
-		handler            http.Handler
+		handler            func(*testing.T) http.Handler
 		requestBody        io.Reader
 		expectedStatusCode int
 	}{
 		{
 			name: "implements flusher",
-			handler: otelhttp.NewHandler(
-				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					assert.Implements(t, (*http.Flusher)(nil), w)
-				}), "test_handler",
-			),
+			handler: func(t *testing.T) http.Handler {
+				return otelhttp.NewHandler(
+					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						assert.Implements(t, (*http.Flusher)(nil), w)
+					}), "test_handler",
+				)
+			},
 			expectedStatusCode: 200,
 		},
 		{
 			name: "succeeds",
-			handler: otelhttp.NewHandler(
-				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					assert.NotNil(t, r.Body)
+			handler: func(t *testing.T) http.Handler {
+				return otelhttp.NewHandler(
+					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						assert.NotNil(t, r.Body)
 
-					b, err := io.ReadAll(r.Body)
-					assert.NoError(t, err)
-					assert.Equal(t, "hello world", string(b))
-				}), "test_handler",
-			),
+						b, err := io.ReadAll(r.Body)
+						assert.NoError(t, err)
+						assert.Equal(t, "hello world", string(b))
+					}), "test_handler",
+				)
+			},
 			requestBody:        strings.NewReader("hello world"),
 			expectedStatusCode: 200,
 		},
 		{
 			name: "succeeds with a nil body",
-			handler: otelhttp.NewHandler(
-				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					assert.Nil(t, r.Body)
-				}), "test_handler",
-			),
+			handler: func(t *testing.T) http.Handler {
+				return otelhttp.NewHandler(
+					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						assert.Nil(t, r.Body)
+					}), "test_handler",
+				)
+			},
 			expectedStatusCode: 200,
 		},
 		{
 			name: "succeeds with an http.NoBody",
-			handler: otelhttp.NewHandler(
-				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					assert.Equal(t, http.NoBody, r.Body)
-				}), "test_handler",
-			),
+			handler: func(t *testing.T) http.Handler {
+				return otelhttp.NewHandler(
+					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						assert.Equal(t, http.NoBody, r.Body)
+					}), "test_handler",
+				)
+			},
 			requestBody:        http.NoBody,
 			expectedStatusCode: 200,
 		},
@@ -83,7 +91,7 @@ func TestHandler(t *testing.T) {
 			require.NoError(t, err)
 
 			rr := httptest.NewRecorder()
-			tc.handler.ServeHTTP(rr, r)
+			tc.handler(t).ServeHTTP(rr, r)
 			assert.Equal(t, tc.expectedStatusCode, rr.Result().StatusCode)
 		})
 	}
