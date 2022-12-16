558a62126f0c17288724afde18854302240e2efd
Alan Protasio <approtas@amazon.com>
2022-12-16 11:22:50 -0800

    Changelog + rename
    Signed-off-by: Alan Protasio <approtas@amazon.com>

--- samplers/aws/xray/traceid_ratio_sampler.go
+++ samplers/aws/xray/traceid_ratio_sampler.go
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package xray
 
 import (
@@ -9,30 +23,30 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
-type xrayTraceIDRatioBasedSampler struct {
+type traceIDRatioBasedSampler struct {
 	sdktrace.Sampler
 	max  uint64
 	desc string
 }
 
-// NewXrayTraceIDRatioBased creates a sampler based on random number.
+// NewTraceIDRatioBased creates a sampler based on random number.
 // fraction parameter should be between 0 and 1 where:
 // fraction >= 1 it will always sample
 // fraction <= 0 it will never sample
-func NewXrayTraceIDRatioBased(fraction float64) sdktrace.Sampler {
+func NewTraceIDRatioBased(fraction float64) sdktrace.Sampler {
 	if fraction >= 1 {
 		return sdktrace.AlwaysSample()
 	} else if fraction <= 0 {
 		return sdktrace.NeverSample()
 	}
 
-	return &xrayTraceIDRatioBasedSampler{
+	return &traceIDRatioBasedSampler{
 		desc: fmt.Sprintf("xrayTraceIDRatioBasedSampler{%v}", fraction),
 		max:  uint64(fraction * math.MaxUint64),
 	}
 }
 
-func (s *xrayTraceIDRatioBasedSampler) ShouldSample(p sdktrace.SamplingParameters) sdktrace.SamplingResult {
+func (s *traceIDRatioBasedSampler) ShouldSample(p sdktrace.SamplingParameters) sdktrace.SamplingResult {
 	// The default otel sampler pick the first 8 bytes to make the sampling decision and this is a problem to
 	// xray case as the first 4 bytes on the xray traceId is the time of the original request and the random part are
 	// the 12 last bytes, and so, this sampler pick the last 8 bytes to make the sampling decision.
@@ -54,6 +68,6 @@ func (s *xrayTraceIDRatioBasedSampler) ShouldSample(p sdktrace.SamplingParameter
 	}
 }
 
-func (s *xrayTraceIDRatioBasedSampler) Description() string {
+func (s *traceIDRatioBasedSampler) Description() string {
 	return s.desc
 }
