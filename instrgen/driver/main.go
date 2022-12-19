01ffca64cbd13d37f83e2879149102d1cc73e6cb
Przemek Delewski <pdelewski@users.noreply.github.com>
2022-12-19 13:29:18 +0100

    Add instrgen implementation

---
+++ instrgen/driver/main.go
@@ -0,0 +1,168 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"go/ast"
+	"log"
+	"os"
+
+	alib "go.opentelemetry.io/contrib/instrgen/lib"
+)
+
+func usage() error {
+	fmt.Println("\nusage driver --command [path to go project] [package pattern]")
+	fmt.Println("\tcommand:")
+	fmt.Println("\t\tinject                                 (injects open telemetry calls into project code)")
+	fmt.Println("\t\tinject-dump-ir                         (injects open telemetry calls into project code and intermediate passes)")
+	fmt.Println("\t\tprune                                  (prune open telemetry calls")
+	fmt.Println("\t\tdumpcfg                                (dumps control flow graph)")
+	fmt.Println("\t\trootfunctions                          (dumps root functions)")
+	return nil
+}
+
+func makeAnalysis(projectPath string, packagePattern string, debug bool) *alib.PackageAnalysis {
+	var rootFunctions []alib.FuncDescriptor
+
+	interfaces := alib.FindInterfaces(projectPath, packagePattern)
+	rootFunctions = append(rootFunctions, alib.FindRootFunctions(projectPath, packagePattern, "AutotelEntryPoint")...)
+	funcDecls := alib.FindFuncDecls(projectPath, packagePattern, interfaces)
+	backwardCallGraph := alib.BuildCallGraph(projectPath, packagePattern, funcDecls, interfaces)
+	fmt.Println("\n\tchild parent")
+	for k, v := range backwardCallGraph {
+		fmt.Print("\n\t", k)
+		fmt.Print(" ", v)
+	}
+	fmt.Println("")
+	analysis := &alib.PackageAnalysis{
+		ProjectPath:    projectPath,
+		PackagePattern: packagePattern,
+		RootFunctions:  rootFunctions,
+		FuncDecls:      funcDecls,
+		Callgraph:      backwardCallGraph,
+		Interfaces:     interfaces,
+		Debug:          debug}
+	return analysis
+}
+
+// Prune.
+func Prune(projectPath string, packagePattern string, debug bool) ([]*ast.File, error) {
+	analysis := makeAnalysis(projectPath, packagePattern, debug)
+	return analysis.Execute(&alib.OtelPruner{}, otelPrunerPassSuffix)
+}
+
+func makeCallGraph(projectPath string, packagePattern string) map[alib.FuncDescriptor][]alib.FuncDescriptor {
+	var funcDecls map[alib.FuncDescriptor]bool
+	var backwardCallGraph map[alib.FuncDescriptor][]alib.FuncDescriptor
+
+	interfaces := alib.FindInterfaces(projectPath, packagePattern)
+	funcDecls = alib.FindFuncDecls(projectPath, packagePattern, interfaces)
+	backwardCallGraph = alib.BuildCallGraph(projectPath, packagePattern, funcDecls, interfaces)
+	return backwardCallGraph
+}
+
+func makeRootFunctions(projectPath string, packagePattern string) []alib.FuncDescriptor {
+	var rootFunctions []alib.FuncDescriptor
+	rootFunctions = append(rootFunctions, alib.FindRootFunctions(projectPath, packagePattern, "AutotelEntryPoint")...)
+	return rootFunctions
+}
+
+func dumpCallGraph(callGraph map[alib.FuncDescriptor][]alib.FuncDescriptor) {
+	fmt.Println("\n\tchild parent")
+	for k, v := range callGraph {
+		fmt.Print("\n\t", k)
+		fmt.Print(" ", v)
+	}
+}
+
+func dumpRootFunctions(rootFunctions []alib.FuncDescriptor) {
+	fmt.Println("rootfunctions:")
+	for _, fun := range rootFunctions {
+		fmt.Println("\t" + fun.TypeHash())
+	}
+}
+
+// Parsing algorithm works as follows. It goes through all function
+// decls and infer function bodies to find call to AutotelEntryPoint
+// A parent function of this call will become root of instrumentation
+// Each function call from this place will be instrumented automatically.
+func executeCommand(command string, projectPath string, packagePattern string) error {
+	if command == "--inject" {
+		_, err := Prune(projectPath, packagePattern, false)
+		if err != nil {
+			return err
+		}
+		analysis := makeAnalysis(projectPath, packagePattern, false)
+		err = ExecutePasses(analysis)
+		if err != nil {
+			return err
+		}
+		fmt.Println("\tinstrumentation done")
+		return nil
+	}
+	if command == "--inject-dump-ir" {
+		_, err := Prune(projectPath, packagePattern, true)
+		if err != nil {
+			return err
+		}
+		analysis := makeAnalysis(projectPath, packagePattern, true)
+		err = ExecutePassesDumpIr(analysis)
+		if err != nil {
+			return err
+		}
+		fmt.Println("\tinstrumentation done")
+		return nil
+	}
+	if command == "--dumpcfg" {
+		backwardCallGraph := makeCallGraph(projectPath, packagePattern)
+		dumpCallGraph(backwardCallGraph)
+		return nil
+	}
+	if command == "--rootfunctions" {
+		rootFunctions := makeRootFunctions(projectPath, packagePattern)
+		dumpRootFunctions(rootFunctions)
+		return nil
+	}
+	if command == "--prune" {
+		_, err := Prune(projectPath, packagePattern, false)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("unknown command")
+}
+
+func checkArgs(args []string) error {
+	if len(args) != 4 {
+		_ = usage()
+		return errors.New("wrong arguments")
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("autotel compiler")
+	err := checkArgs(os.Args)
+	if err != nil {
+		return
+	}
+	err = executeCommand(os.Args[1], os.Args[2], os.Args[3])
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ instrgen/lib/instrumentation.go
@@ -0,0 +1,380 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lib // import "go.opentelemetry.io/contrib/instrgen/lib"
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+
+	"golang.org/x/tools/go/packages"
+)
+
+// InstrumentationPass.
+type InstrumentationPass struct {
+}
+
+func makeInitStmts(name string) []ast.Stmt {
+	childTracingSupress := &ast.AssignStmt{
+		Lhs: []ast.Expr{
+			&ast.Ident{
+				Name: "_",
+			},
+		},
+		Tok: token.ASSIGN,
+		Rhs: []ast.Expr{
+			&ast.Ident{
+				Name: "__atel_child_tracing_ctx",
+			},
+		},
+	}
+	s1 :=
+		&ast.AssignStmt{
+			Lhs: []ast.Expr{
+				&ast.Ident{
+					Name: "__atel_ts",
+				},
+			},
+			Tok: token.DEFINE,
+
+			Rhs: []ast.Expr{
+				&ast.CallExpr{
+					Fun: &ast.SelectorExpr{
+						X: &ast.Ident{
+							Name: "rtlib",
+						},
+						Sel: &ast.Ident{
+							Name: "NewTracingState",
+						},
+					},
+					Lparen:   54,
+					Ellipsis: 0,
+				},
+			},
+		}
+	s2 := &ast.DeferStmt{
+		Defer: 27,
+		Call: &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X: &ast.Ident{
+					Name: "rtlib",
+				},
+				Sel: &ast.Ident{
+					Name: "Shutdown",
+				},
+			},
+			Lparen: 48,
+			Args: []ast.Expr{
+				&ast.Ident{
+					Name: "__atel_ts",
+				},
+			},
+			Ellipsis: 0,
+		},
+	}
+
+	s3 := &ast.ExprStmt{
+		X: &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X: &ast.Ident{
+					Name: "__atel_otel",
+				},
+				Sel: &ast.Ident{
+					Name: "SetTracerProvider",
+				},
+			},
+			Lparen: 49,
+			Args: []ast.Expr{
+				&ast.SelectorExpr{
+					X: &ast.Ident{
+						Name: "__atel_ts",
+					},
+					Sel: &ast.Ident{
+						Name: "Tp",
+					},
+				},
+			},
+			Ellipsis: 0,
+		},
+	}
+	s4 := &ast.AssignStmt{
+		Lhs: []ast.Expr{
+			&ast.Ident{
+				Name: "__atel_ctx",
+			},
+		},
+		Tok: token.DEFINE,
+		Rhs: []ast.Expr{
+			&ast.CallExpr{
+				Fun: &ast.SelectorExpr{
+					X: &ast.Ident{
+						Name: "__atel_context",
+					},
+					Sel: &ast.Ident{
+						Name: "Background",
+					},
+				},
+				Lparen:   52,
+				Ellipsis: 0,
+			},
+		},
+	}
+	s5 := &ast.AssignStmt{
+		Lhs: []ast.Expr{
+			&ast.Ident{
+				Name: "__atel_child_tracing_ctx",
+			},
+			&ast.Ident{
+				Name: "__atel_span",
+			},
+		},
+		Tok: token.DEFINE,
+		Rhs: []ast.Expr{
+			&ast.CallExpr{
+				Fun: &ast.SelectorExpr{
+					X: &ast.CallExpr{
+						Fun: &ast.SelectorExpr{
+							X: &ast.Ident{
+								Name: "__atel_otel",
+							},
+							Sel: &ast.Ident{
+								Name: "Tracer",
+							},
+						},
+						Lparen: 50,
+						Args: []ast.Expr{
+							&ast.Ident{
+								Name: `"` + name + `"`,
+							},
+						},
+						Ellipsis: 0,
+					},
+					Sel: &ast.Ident{
+						Name: "Start",
+					},
+				},
+				Lparen: 62,
+				Args: []ast.Expr{
+					&ast.Ident{
+						Name: "__atel_ctx",
+					},
+					&ast.Ident{
+						Name: `"` + name + `"`,
+					},
+				},
+				Ellipsis: 0,
+			},
+		},
+	}
+
+	s6 := &ast.DeferStmt{
+		Defer: 27,
+		Call: &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X: &ast.Ident{
+					Name: "__atel_span",
+				},
+				Sel: &ast.Ident{
+					Name: "End",
+				},
+			},
+			Lparen:   41,
+			Ellipsis: 0,
+		},
+	}
+	stmts := []ast.Stmt{s1, s2, s3, s4, s5, childTracingSupress, s6}
+	return stmts
+}
+
+func makeSpanStmts(name string, paramName string) []ast.Stmt {
+	s1 := &ast.AssignStmt{
+		Lhs: []ast.Expr{
+			&ast.Ident{
+				Name: "__atel_child_tracing_ctx",
+			},
+			&ast.Ident{
+				Name: "__atel_span",
+			},
+		},
+		Tok: token.DEFINE,
+		Rhs: []ast.Expr{
+			&ast.CallExpr{
+				Fun: &ast.SelectorExpr{
+					X: &ast.CallExpr{
+						Fun: &ast.SelectorExpr{
+							X: &ast.Ident{
+								Name: "__atel_otel",
+							},
+							Sel: &ast.Ident{
+								Name: "Tracer",
+							},
+						},
+						Lparen: 50,
+						Args: []ast.Expr{
+							&ast.Ident{
+								Name: `"` + name + `"`,
+							},
+						},
+						Ellipsis: 0,
+					},
+					Sel: &ast.Ident{
+						Name: "Start",
+					},
+				},
+				Lparen: 62,
+				Args: []ast.Expr{
+					&ast.Ident{
+						Name: paramName,
+					},
+					&ast.Ident{
+						Name: `"` + name + `"`,
+					},
+				},
+				Ellipsis: 0,
+			},
+		},
+	}
+
+	s2 := &ast.AssignStmt{
+		Lhs: []ast.Expr{
+			&ast.Ident{
+				Name: "_",
+			},
+		},
+		Tok: token.ASSIGN,
+		Rhs: []ast.Expr{
+			&ast.Ident{
+				Name: "__atel_child_tracing_ctx",
+			},
+		},
+	}
+
+	s3 := &ast.DeferStmt{
+		Defer: 27,
+		Call: &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X: &ast.Ident{
+					Name: "__atel_span",
+				},
+				Sel: &ast.Ident{
+					Name: "End",
+				},
+			},
+			Lparen:   41,
+			Ellipsis: 0,
+		},
+	}
+	stmts := []ast.Stmt{s1, s2, s3}
+	return stmts
+}
+
+// Execute.
+func (pass *InstrumentationPass) Execute(
+	node *ast.File,
+	analysis *PackageAnalysis,
+	pkg *packages.Package,
+	pkgs []*packages.Package) []Import {
+	var imports []Import
+	addImports := false
+	addContext := false
+	// store all function literals positions
+	// that are part of assignment statement
+	// it's used to avoid injection into literal
+	// more than once
+	var functionLiteralPositions []token.Pos
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.FuncDecl:
+			pkgPath := GetPkgPathForFunction(pkg, pkgs, x, analysis.Interfaces)
+			fundId := pkgPath + "." + pkg.TypesInfo.Defs[x.Name].Name()
+			fun := FuncDescriptor{
+				Id:              fundId,
+				DeclType:        pkg.TypesInfo.Defs[x.Name].Type().String(),
+				CustomInjection: false}
+			// check if it's root function or
+			// one of function in call graph
+			// and emit proper ast nodes
+			_, exists := analysis.Callgraph[fun]
+			if !exists {
+				if !Contains(analysis.RootFunctions, fun) {
+					return false
+				}
+			}
+			for _, root := range analysis.RootFunctions {
+				visited := map[FuncDescriptor]bool{}
+				fmt.Println("\t\t\tInstrumentation FuncDecl:", fundId, pkg.TypesInfo.Defs[x.Name].Type().String())
+				if isPath(analysis.Callgraph, fun, root, visited) && fun.TypeHash() != root.TypeHash() {
+					x.Body.List = append(makeSpanStmts(x.Name.Name, "__atel_tracing_ctx"), x.Body.List...)
+					addContext = true
+					addImports = true
+				} else {
+					// check whether this function is root function
+					if !Contains(analysis.RootFunctions, fun) {
+						return false
+					}
+					x.Body.List = append(makeInitStmts(x.Name.Name), x.Body.List...)
+					addContext = true
+					addImports = true
+				}
+			}
+		case *ast.AssignStmt:
+			for _, e := range x.Lhs {
+				if ident, ok := e.(*ast.Ident); ok {
+					_ = ident
+					pkgPath := ""
+					pkgPath = GetPkgNameFromDefsTable(pkg, ident)
+					if pkg.TypesInfo.Defs[ident] == nil {
+						return false
+					}
+					fundId := pkgPath + "." + pkg.TypesInfo.Defs[ident].Name()
+					fun := FuncDescriptor{
+						Id:              fundId,
+						DeclType:        pkg.TypesInfo.Defs[ident].Type().String(),
+						CustomInjection: true}
+					_, exists := analysis.Callgraph[fun]
+					if exists {
+						return false
+					}
+				}
+			}
+			for _, e := range x.Rhs {
+				if funLit, ok := e.(*ast.FuncLit); ok {
+					functionLiteralPositions = append(functionLiteralPositions, funLit.Pos())
+					funLit.Body.List = append(makeSpanStmts("anonymous", "__atel_child_tracing_ctx"), funLit.Body.List...)
+					addImports = true
+					addContext = true
+				}
+			}
+		case *ast.FuncLit:
+			for _, pos := range functionLiteralPositions {
+				if pos == x.Pos() {
+					return false
+				}
+			}
+			x.Body.List = append(makeSpanStmts("anonymous", "__atel_child_tracing_ctx"), x.Body.List...)
+			addImports = true
+			addContext = true
+		}
+
+		return true
+	})
+	if addContext {
+		imports = append(imports, Import{"__atel_context", "context", Add})
+	}
+	if addImports {
+		imports = append(imports, Import{"__atel_otel", "go.opentelemetry.io/otel", Add})
+	}
+	return imports
+}
