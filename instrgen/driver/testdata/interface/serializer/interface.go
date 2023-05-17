560e997a4a0237bfe5f409b403de45dad4cb0678
Robert Pająk <pellared@hotmail.com>
2023-05-17 11:46:35 +0200

    Add missing vanity imports

--- instrgen/driver/testdata/interface/serializer/interface.go
+++ instrgen/driver/testdata/interface/serializer/interface.go
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 //nolint:all // Linter is executed at the same time as tests which leads to race conditions and failures.
-package serializer
+package serializer // import "go.opentelemetry.io/contrib/instrgen/testdata/interface/serializer"
 
 type Serializer interface {
 	Serialize()
