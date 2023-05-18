8e3be4cf30532c90071c1610826c0757e0ccc56e
Robert Pająk <pellared@hotmail.com>
2023-05-18 20:50:34 +0200

    Fix netconv.go.tmpl

--- instrumentation/net/http/httptrace/otelhttptrace/internal/semconvutil/netconv.go
+++ instrumentation/net/http/httptrace/otelhttptrace/internal/semconvutil/netconv.go
@@ -1,8 +1,7 @@
-// Copyright The OpenTelemetry Authors
-//
 // Code created by gotmpl. DO NOT MODIFY.
 // source: internal/shared/semconvutil/netconv.go.tmpl
 
+// Copyright The OpenTelemetry Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
