5b8181cc96081b26c4436dc7abe4db35a7b4ac94
Robert Pająk <pellared@hotmail.com>
2023-05-25 16:59:41 +0200

    Remove doc.go.tmpl

--- instrumentation/github.com/gorilla/mux/otelmux/internal/semconvutil/gen.go
+++ instrumentation/github.com/gorilla/mux/otelmux/internal/semconvutil/gen.go
@@ -15,7 +15,6 @@
 package semconvutil // import "go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux/internal/semconvutil"
 
 // Generate semconvutil package:
-//go:generate gotmpl --body=../../../../../../../internal/shared/semconvutil/doc.go.tmpl "--data={}" --out=doc.go
 //go:generate gotmpl --body=../../../../../../../internal/shared/semconvutil/httpconv_test.go.tmpl "--data={}" --out=httpconv_test.go
 //go:generate gotmpl --body=../../../../../../../internal/shared/semconvutil/httpconv.go.tmpl "--data={}" --out=httpconv.go
 //go:generate gotmpl --body=../../../../../../../internal/shared/semconvutil/netconv_test.go.tmpl "--data={}" --out=netconv_test.go
