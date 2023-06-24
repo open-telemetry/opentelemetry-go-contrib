521265324bac5fd94af3116b8e4e4f843c6aaaf4
Trevor Foster <trevor.foster@hotmail.ca>
2023-06-23 23:15:04 -0400

    Add gorilla/mux otelhttp server example

--- instrumentation/net/http/otelhttp/example/server/github.com/gorilla/mux/go.mod
+++ instrumentation/net/http/otelhttp/example/server/github.com/gorilla/mux/go.mod
@@ -1,11 +1,11 @@
-module go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp/example/server/github.com/labstack/echo
+module go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp/example/server/github.com/gorilla/mux
 
 go 1.18
 
 replace go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp => ../../../../../
 
 require (
-	github.com/labstack/echo/v4 v4.10.2
+	github.com/gorilla/mux v1.8.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.42.0
 	go.opentelemetry.io/otel v1.16.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.16.0
@@ -17,14 +17,6 @@ require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/labstack/gommon v0.4.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
 )
---
+++ instrumentation/net/http/otelhttp/example/server/github.com/gorilla/mux/server.go
@@ -0,0 +1,83 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	oteltrace "go.opentelemetry.io/otel/trace"
+)
+
+var tracer = otel.Tracer("mux-server")
+
+func main() {
+	tp, err := initTracer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+	r := mux.NewRouter()
+	r.Use(otelhttp.NewMiddleware("my-server"))
+
+	p := r.Path("/users/{id:[0-9]+}").Subrouter()
+	p.Use(otelhttp.WithRouteTagMiddleware("/users/:id"))
+	p.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		name := getUser(r.Context(), id)
+		reply := fmt.Sprintf("user %s (id %s)\n", name, id)
+		_, _ = w.Write(([]byte)(reply))
+	})
+
+	_ = http.ListenAndServe(":8080", r)
+}
+
+func initTracer() (*sdktrace.TracerProvider, error) {
+	exporter, err := stdout.New(stdout.WithPrettyPrint())
+	if err != nil {
+		return nil, err
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp, nil
+}
+
+func getUser(ctx context.Context, id string) string {
+	_, span := tracer.Start(ctx, "getUser", oteltrace.WithAttributes(attribute.String("id", id)))
+	defer span.End()
+	if id == "123" {
+		return "otelmux tester"
+	}
+	return "unknown"
+}
