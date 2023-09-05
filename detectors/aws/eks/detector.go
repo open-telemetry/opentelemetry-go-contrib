53f4e7ebd6ffd34bbe63581963c7880f9bf91ac9
Tyler Yahn <MrAlias@users.noreply.github.com>
2023-09-05 10:52:40 -0700

    Update AWS detector and instrumentation semconv to v1.21.0 (#4265)
    * Update AWS detector semconv
    
    * Update PR number
    
    * Upgrade otellambda semconv
    
    * Upgrade otelaws

--- detectors/aws/eks/detector.go
+++ detectors/aws/eks/detector.go
@@ -27,7 +27,7 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
 const (
