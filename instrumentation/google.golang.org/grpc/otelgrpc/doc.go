0213f6bca8a5b0769da1c81a369a91fe3f9d92cc
Ziqi Zhao <zhaoziqi9146@gmail.com>
2023-09-07 17:56:08 +0800

    fix typo
    Signed-off-by: Ziqi Zhao <zhaoziqi9146@gmail.com>

--- instrumentation/google.golang.org/grpc/otelgrpc/doc.go
+++ instrumentation/google.golang.org/grpc/otelgrpc/doc.go
@@ -26,20 +26,20 @@ We strongly still recommand you to use [stats.Handler], mainly for two reasons:
 
 Functional advantages: [stats.Handler] has more information for user to build more flexible and granular metric, for example
 
-  - multiple different types of represent "data length": In [stats.InPayLoad], there exists "Length", "CompressedLength", "WireLength" to denote the size of uncompressed, compressed payload data, with or without framing data. But in interceptors, we can only got uncompressed data, and this feature is also removed due to performance problem.
+  - multiple different types of represent "data length": In [stats.InPayload], there exists "Length", "CompressedLength", "WireLength" to denote the size of uncompressed, compressed payload data, with or without framing data. But in interceptors, we can only got uncompressed data, and this feature is also removed due to performance problem.
 
-  - more accurate timestamp: [stats.InPayLoad]'s "RecvTime" and [stats.OutPayLoad]'s "SentTime" records more accurate timestamp that server got and sent the message, the timestamp recorded by interceptors depends on the location of this interceptors in the total interceptor chain.
+  - more accurate timestamp: [stats.InPayload]'s "RecvTime" and [stats.OutPayload]'s "SentTime" records more accurate timestamp that server got and sent the message, the timestamp recorded by interceptors depends on the location of this interceptors in the total interceptor chain.
 
   - some other use cases: for example, catch failure of decoding message.
 
 Performance advantages: If too many interceptors are registered in a service, the interceptor chain can become too long, which increases the latency and processing time of the entire RPC call.
 
 [stats.Handler]: https://pkg.go.dev/google.golang.org/grpc/stats#Handler
-[stats.InPayload]: https://pkg.go.dev/google.golang.org/grpc/stats#OutPayload
-[stats.OutPayload]: https://pkg.go.dev/google.golang.org/grpc/stats#InPayload
 [grpc.UnaryClientInterceptor]: https://pkg.go.dev/google.golang.org/grpc#UnaryClientInterceptor
 [grpc.UnaryServerInterceptor]: https://pkg.go.dev/google.golang.org/grpc#UnaryServerInterceptor
 [grpc.StreamClientInterceptor]: https://pkg.go.dev/google.golang.org/grpc#StreamClientInterceptor
 [grpc.StreamServerInterceptor]: https://pkg.go.dev/google.golang.org/grpc#StreamServerInterceptor
+[stats.OutPayload]: https://pkg.go.dev/google.golang.org/grpc/stats#OutPayload
+[stats.InPayload]: https://pkg.go.dev/google.golang.org/grpc/stats#InPayload
 */
 package otelgrpc // import "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
