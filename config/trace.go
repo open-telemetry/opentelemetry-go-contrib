e874abc12c73b5dc81634ad02e6f93fb0ce5ce3b
Robert Pająk <pellared@hotmail.com>
2023-11-10 18:18:33 +0100

    config: Do not use deprecated API

--- config/trace.go
+++ config/trace.go
@@ -6,11 +6,12 @@ package config // import "go.opentelemetry.io/contrib/config"
 import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
 )
 
 func initTracerProvider(cfg configOptions) trace.TracerProvider {
 	if cfg.opentelemetryConfig.TracerProvider == nil {
-		return trace.NewNoopTracerProvider()
+		return noop.NewTracerProvider()
 	}
 	return sdktrace.NewTracerProvider()
 }
