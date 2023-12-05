2a310bd319d207ea86eb8a812517843f4bcd420e
Ben Carr <carrbens@gmail.com>
2023-12-04 20:35:38 -0800

    return existing ctx if it can't be canceled

--- instrumentation/net/http/otelhttp/handler.go
+++ instrumentation/net/http/otelhttp/handler.go
@@ -284,11 +284,11 @@ func WithRouteTag(route string, h http.Handler) http.Handler {
 	})
 }
 
-func withoutCancel(parent context.Context) context.Context {
-	if parent == nil {
-		panic("cannot create context from nil parent")
+func withoutCancel(ctx context.Context) context.Context {
+	if ctx.Done() == nil {
+		return ctx
 	}
-	return withoutCancelCtx{parent}
+	return withoutCancelCtx{ctx}
 }
 
 type withoutCancelCtx struct {
