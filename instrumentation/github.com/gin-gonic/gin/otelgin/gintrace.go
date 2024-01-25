a91fa5c5c3579a36382ed4fbcd32b497c250708c
rehanpfmr <111350825+rehanpfmr@users.noreply.github.com>
2024-01-25 12:26:38 +0530

    Update gintrace.go

--- instrumentation/github.com/gin-gonic/gin/otelgin/gintrace.go
+++ instrumentation/github.com/gin-gonic/gin/otelgin/gintrace.go
@@ -55,7 +55,7 @@ func Middleware(service string, opts ...Option) gin.HandlerFunc {
 	}
 	return func(c *gin.Context) {
 		for _, f := range cfg.Filters {
-			if !f(c) {
+			if !f(c.Request) {
 				// Serve the request to the next middleware
 				// if a filter rejects the request.
 				c.Next()
