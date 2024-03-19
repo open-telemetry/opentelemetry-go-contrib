6a54c94bb12dd49a727cff6f0b41c01c4c851a05
Tyler Yahn <codingalias@gmail.com>
2024-03-19 11:23:38 -0700

    Add Version func

---
+++ bridges/otelslog/version.go
@@ -0,0 +1,9 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package otelslog // import "go.opentelemetry.io/contrib/bridges/otelslog"
+
+// Version is the current release version of otelslog in use.
+func Version() string {
+	return "0.0.1-alpha"
+}
