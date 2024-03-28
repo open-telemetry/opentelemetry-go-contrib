00fbb0de0acd9d068ca26c3ec74a4586707f4e9a
Tyler Yahn <codingalias@gmail.com>
2024-03-28 11:47:20 -0700

    Add Go Authors copyright

--- bridges/otelslog/handler_test.go
+++ bridges/otelslog/handler_test.go
@@ -1,6 +1,10 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package otelslog
 
 import (
