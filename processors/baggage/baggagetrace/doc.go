31c8398eb800aa47c32b4acaa8bdd4f229c62a0f
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-05-06 15:29:11 -0700

    processors/baggage: add baggage span processor (#5404)
    * processors/baggage: add baggage span processor
    
    Fixes #5397
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * fix lint issues + dependabot generate
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * apply review feedback
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * more review feedback
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * Update processors/baggage/processor_test.go
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    
    * lint
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * feedback from review
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * rename baggage -> baggagetrace
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * more fixes
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * docs
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * lint fixes
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * generate
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Damien Mathieu <42@dmathieu.com>
    
    * Update CHANGELOG.md
    
    * Update processors/baggage/baggagetrace/doc.go
    
    * Apply suggestions from code review
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    
    * Update processors/baggage/baggagetrace/processor.go
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    
    * Apply suggestions from code review
    
    * Update CHANGELOG.md
    
    * Update CODEOWNERS
    
    Co-authored-by: Mike Goldsmith <goldsmith.mike@gmail.com>
    
    ---------
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    Co-authored-by: Damien Mathieu <42@dmathieu.com>
    Co-authored-by: Mike Goldsmith <goldsmith.mike@gmail.com>
    Co-authored-by: Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>

---
+++ processors/baggage/baggagetrace/doc.go
@@ -0,0 +1,5 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package baggagetrace implements a baggage span processor.
+package baggagetrace // import "go.opentelemetry.io/contrib/processors/baggage/baggagetrace"
