685a3c80b4a5fbd6136037d85c593a162b4b8531
Tyler Yahn <codingalias@gmail.com>
2024-05-22 09:57:31 -0700

    Deprecate otellambda

--- instrumentation/github.com/aws/aws-lambda-go/otellambda/xrayconfig/xrayconfig.go
+++ instrumentation/github.com/aws/aws-lambda-go/otellambda/xrayconfig/xrayconfig.go
@@ -1,6 +1,14 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+// Package xrayconfig provides configuration for the otellambda package.
+//
+// Deprecated: otellambda has no Code Owner.
+// After August 21, 2024, it may no longer be supported and may stop
+// receiving new releases unless a new Code Owner is found. See
+// [this issue] if you would like to become the Code Owner of this module.
+//
+// [this issue]: https://github.com/open-telemetry/opentelemetry-go-contrib/issues/5546
 package xrayconfig // import "go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda/xrayconfig"
 
 import (
