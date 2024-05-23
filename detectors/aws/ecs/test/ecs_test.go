3f966e6df91b897d3df6acbe12f942662ae45329
Tyler Yahn <codingalias@gmail.com>
2024-05-23 11:20:05 -0700

    Revert "Deprecate the AWS ECS detector (#5638)"
    This reverts commit 8b96509f58656cd61bd393f5201fd25f0ff748b2.

--- detectors/aws/ecs/test/ecs_test.go
+++ detectors/aws/ecs/test/ecs_test.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"testing"
 
-	ecs "go.opentelemetry.io/contrib/detectors/aws/ecs" // nolint:staticcheck  // deprecated.
+	ecs "go.opentelemetry.io/contrib/detectors/aws/ecs"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
