2c87ebce0644def538843d5be581cb1d48208df8
Khushi Jain <khushij393@gmail.com>
2024-06-03 15:38:27 +0530

    Update bridges/otelzap/core_test.go
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- bridges/otelzap/core_test.go
+++ bridges/otelzap/core_test.go
@@ -44,8 +44,8 @@ func TestCore(t *testing.T) {
 
 	rec.Reset()
 
-	// TODO WriteContext
-	// TOOD WithContext
+	// TODO: Add WriteContext test case
+	// TOOD: Add WithContext test case
 
 	// test child logger with accumulated fields
 	t.Run("With", func(t *testing.T) {
