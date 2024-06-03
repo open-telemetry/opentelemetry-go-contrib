63d85a6f795f7f84caa38d4527f8585f96c3e38d
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-06-03 14:11:56 +0100

    move usage and examples into doc.go

--- processors/baggage/baggagetrace/doc.go
+++ processors/baggage/baggagetrace/doc.go
@@ -2,4 +2,56 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package baggagetrace implements a baggage span processor.
+//
+// This is an OpenTelemetry [Span Processor] that reads key/values stored in
+// [Baggage] in the starting span's parent context and adds them as attributes
+// to the span.
+//
+// Keys and values added to Baggage will appear on all subsequent child spans for
+// a trace within this service *and* will be propagated to external services via
+// propagation headers.
+// If the external services also have a Baggage span processor, the keys and
+// values will appear in those child spans as well.
+//
+// ⚠️ Waning ⚠️
+// To repeat: a consequence of adding data to Baggage is that the keys and values
+// will appear in all outgoing HTTP baggage headers from the application.
+//
+// Do not put sensitive information in Baggage.
+//
+// # Usage
+//
+// Add the span processor when configuring the tracer provider.
+//
+// To configure the span processor to copy all baggage entries during
+// configuration:
+//
+//	import (
+//		"go.opentelemetry.io/contrib/processors/baggage/baggagetrace"
+//	)
+//
+//	tp := trace.NewTracerProvider(
+//		trace.WithSpanProcessor(baggagetrace.New(baggagetrace.AllowAllBaggageKeys)),
+//		// ...
+//	)
+//
+// Alternatively, you can provide a custom baggage key predicate to select
+// which baggage keys you want to copy.
+//
+// For example, to only copy baggage entries that start with 'my-key':
+//
+//	baggagetrace.New(func(baggageKey string) bool {
+//		return strings.HasPrefix(baggageKey, "my-key")
+//	})
+//
+// For example, to only copy baggage entries that match the regex '^key.+':
+//
+//	expr := regexp.MustCompile(`^key.+`)
+//
+//	baggagetrace.New(func(baggageKey string) bool {
+//		return expr.MatchString(baggageKey)
+//	})
+//
+// [Span Processor]: https://opentelemetry.io/docs/specs/otel/trace/sdk/#span-processor
+// [Baggage]: https://opentelemetry.io/docs/specs/otel/api/baggage
 package baggagetrace // import "go.opentelemetry.io/contrib/processors/baggage/baggagetrace"
