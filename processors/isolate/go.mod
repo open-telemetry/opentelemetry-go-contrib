24b87d6957433cfcb5894df272a4b178f45489f1
Robert Pająk <pellared@hotmail.com>
2024-07-05 12:12:07 +0200

    processors/isolate: Add NewLogProcessor

---
+++ processors/isolate/go.mod
@@ -0,0 +1,17 @@
+module go.opentelemetry.io/contrib/processors/isolate
+
+go 1.21
+
+require go.opentelemetry.io/otel/sdk/log v0.4.0
+
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/log v0.4.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+)
---
+++ processors/isolate/processor.go
@@ -0,0 +1,58 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package isolate // import "go.opentelemetry.io/contrib/processors/isolate"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/sdk/log"
+)
+
+// NewLogProcessor returns a new [LogProcessor] that wraps the downstream
+// [log.Processor].
+//
+// If downstream is nil a default No-Op [log.Processor] is used. The returned
+// processor will not be enabled for nor emit any records.
+func NewLogProcessor(downstream log.Processor) *LogProcessor {
+	if downstream == nil {
+		downstream = defaultProcessor
+	}
+	return &LogProcessor{Processor: downstream}
+}
+
+// LogProcessor is an [log.Processor] implementation clones the recieved log
+// records in order to no share mutable data with subsequent registered processors.
+//
+// If the wrapped [log.Processor] is nil, calls to the LogProcessor methods
+// will panic.
+//
+// Use [NewLogProcessor] to create a new LogProcessor that ensures
+// no panics.
+type LogProcessor struct {
+	log.Processor
+}
+
+// Compile time assertion that LogProcessor implements log.Processor.
+var _ log.Processor = (*LogProcessor)(nil)
+
+// OnEmit clones the record and calls the wrapped downstream processor.
+func (p *LogProcessor) OnEmit(ctx context.Context, record log.Record) error {
+	record = record.Clone()
+	return p.Processor.OnEmit(ctx, record)
+}
+
+// Enabled clones the record and calls the wrapped downstream processor.
+func (p *LogProcessor) Enabled(ctx context.Context, record log.Record) bool {
+	record = record.Clone()
+	return p.Processor.Enabled(ctx, record)
+}
+
+var defaultProcessor = noopProcessor{}
+
+type noopProcessor struct{}
+
+func (p noopProcessor) OnEmit(context.Context, log.Record) error { return nil }
+func (p noopProcessor) Enabled(context.Context, log.Record) bool { return false }
+func (p noopProcessor) Shutdown(context.Context) error           { return nil }
+func (p noopProcessor) ForceFlush(context.Context) error         { return nil }
