ed611a9ebacd4656ca8d62937f90692e1b82c1af
EROMOSELE AKHIGBE <akhigbeeromo@gmail.com>
2024-07-10 16:20:53 +0100

    fixed lint issues

--- bridges/otelzerolog/example_test.go
+++ bridges/otelzerolog/example_test.go
@@ -2,19 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package otelzerolog_test
-import (
-    "github.com/rs/zerolog"
-    "os"
-     "go.opentelemetry.io/contrib/bridges/otelzerolog"
-    "go.opentelemetry.io/otel/log/noop"
-)
-func Example() {
-    // Use a working LoggerProvider implementation instead e.g. using go.opentelemetry.io/otel/sdk/log.
-    provider := noop.NewLoggerProvider()
 
-    hook := otelzerolog.NewSeverityHook("my/pkg/name", otelzerolog.WithLoggerProvider(provider))
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+
+	"go.opentelemetry.io/contrib/bridges/otelzerolog"
+	"go.opentelemetry.io/otel/log/noop"
+)
+
+func Example() {
+	// Use a working LoggerProvider implementation instead e.g. using go.opentelemetry.io/otel/sdk/log.
+	provider := noop.NewLoggerProvider()
+
+	hook := otelzerolog.NewSeverityHook("my/pkg/name", otelzerolog.WithLoggerProvider(provider))
 
 	logger := zerolog.New(os.Stdout).With().Logger()
-    logger = logger.Hook(hook)
-
+	logger = logger.Hook(hook)
 }
