ff58f84036c6c02ccaa202bd3f69aba5d3d7a81f
Robert Pająk <pellared@hotmail.com>
2024-07-22 09:01:09 +0200

    Update bridges/otelzerolog/hook.go

--- bridges/otelzerolog/hook.go
+++ bridges/otelzerolog/hook.go
@@ -3,8 +3,7 @@
 
 // Package otelzerolog provides a [Hook], a [zerolog.Hook] implementation that
 // can be used to bridge between the [zerolog] API and [OpenTelemetry].
-// Note: otelzerolog is a basic implementation and does not support adding custom fields.
-
+//
 // # Record Conversion
 //
 // The [zerolog.event] records are converted to OpenTelemetry [log.Record] in
@@ -13,6 +12,7 @@
 //   - Message is set as the Body using a [log.StringValue].
 //   - Level is transformed and set as the Severity. The SeverityText is also
 //     set.
+//
 // The Level is transformed to the OpenTelemetry Severity types in the following way.
 //
 //   - [zerolog.DebugLevel] is transformed to [log.SeverityDebug]
@@ -21,8 +21,11 @@
 //   - [zerolog.ErrorLevel] is transformed to [log.SeverityError]
 //   - [zerolog.PanicLevel] is transformed to [log.SeverityFatal1]
 //   - [zerolog.FatalLevel] is transformed to [log.SeverityFatal2]
+//
+//
+// NOTE: Fields are not transformed because of https://github.com/rs/zerolog/issues/493.
+//
 // [OpenTelemetry]: https://opentelemetry.io/docs/concepts/signals/logs/
-
 package otelzerolog // import "go.opentelemetry.io/contrib/bridges/otelzerolog"
 
 import (
