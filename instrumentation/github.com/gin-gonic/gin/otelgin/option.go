54aedbe20fe417ab8547caa28636b1b2dc683dcb
rehanpfmr <111350825+rehanpfmr@users.noreply.github.com>
2024-07-30 17:29:04 +0530

    Update option.go
    Signed-off-by: rehanpfmr <111350825+rehanpfmr@users.noreply.github.com>

--- instrumentation/github.com/gin-gonic/gin/otelgin/option.go
+++ instrumentation/github.com/gin-gonic/gin/otelgin/option.go
@@ -67,7 +67,7 @@ func WithTracerProvider(provider oteltrace.TracerProvider) Option {
 
 // WithFilter adds a filter to the list of filters used by the handler.
 // If any filter indicates to exclude a request then the request will not be
-// traced. All filters must allow a request to be traced for a Span to be created.
+// traced. All gin and net/http filters must allow a request to be traced for a Span to be created.
 // If no filters are provided then all requests are traced.
 // Filters will be invoked for each processed request, it is advised to make them
 // simple and fast.
@@ -78,11 +78,6 @@ func WithFilter(f ...Filter) Option {
 }
 
 // WithGinFilter adds a gin filter to the list of filters used by the handler.
-// If any filter indicates to exclude a request then the request will not be
-// traced. All gin and net/http filters must allow a request to be traced for a Span to be created.
-// If no filters are provided then all requests are traced.
-// Filters will be invoked for each processed request, it is advised to make them
-// simple and fast.
 func WithGinFilter(f ...GinFilter) Option {
 	return optionFunc(func(c *config) {
 		c.GinFilters = append(c.GinFilters, f...)
