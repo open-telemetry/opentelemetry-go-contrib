1cf302f3ccdf18c5c20fed8a68cae082b00ef7b3
David Ashpole <dashpole@google.com>
2024-08-07 02:39:20 +0000

    implement go.schedule.duration

--- instrumentation/runtime/test/producer_test.go
+++ instrumentation/runtime/test/producer_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.opentelemetry.io/otel/sdk/instrumentation"
 	"go.opentelemetry.io/otel/sdk/metric"
