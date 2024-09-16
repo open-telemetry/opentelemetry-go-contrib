b8b70476bafc3e5036ac7e0276b4c872e34ca47b
Tyler Yahn <codingalias@gmail.com>
2024-09-16 10:12:11 -0700

    Test SeverityVar concurrent safe

---
+++ processors/minsev/severity_test.go
@@ -0,0 +1,37 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package minsev // import "go.opentelemetry.io/contrib/processors/minsev"
+
+import (
+	"sync"
+	"testing"
+
+	"go.opentelemetry.io/otel/log"
+)
+
+func TestSeverityVarConcurrentSafe(t *testing.T) {
+	var sev SeverityVar
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for s := SeverityUndefined; s <= SeverityFatal4; s++ {
+			sev.Set(s)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var got log.Severity
+		for i := SeverityFatal4 - SeverityUndefined; i >= 0; i-- {
+			got = sev.Severity()
+		}
+		_ = got
+	}()
+
+	wg.Wait()
+}
