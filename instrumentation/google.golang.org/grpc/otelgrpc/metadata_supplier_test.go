9207da413a5c6c0db168fdb68bd5cfb99ade1d01
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-09-25 11:51:34 +0200

    [chore]: enable expected-actual rule from testifylint (#6154)
    Testifylint is a linter that provides best practices with the use of
    testify.
    
    This PR enables
    [expected-actual](https://github.com/Antonboom/testifylint?tab=readme-ov-file#expected-actual)
    rule from [testifylint](https://github.com/Antonboom/testifylint)
    
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- instrumentation/google.golang.org/grpc/otelgrpc/metadata_supplier_test.go
+++ instrumentation/google.golang.org/grpc/otelgrpc/metadata_supplier_test.go
@@ -17,12 +17,12 @@ func TestMetadataSupplier(t *testing.T) {
 	ms := &metadataSupplier{&md}
 
 	v1 := ms.Get("k1")
-	assert.Equal(t, v1, "v1")
+	assert.Equal(t, "v1", v1)
 
 	ms.Set("k2", "v2")
 
 	v1 = ms.Get("k1")
 	v2 := ms.Get("k2")
-	assert.Equal(t, v1, "v1")
-	assert.Equal(t, v2, "v2")
+	assert.Equal(t, "v1", v1)
+	assert.Equal(t, "v2", v2)
 }
