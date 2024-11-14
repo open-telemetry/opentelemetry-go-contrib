3faa941c4d4a39f290d398e956cb7bbdb3843b69
Robert Pająk <pellared@hotmail.com>
2024-11-14 09:33:27 +0100

    Update bridges/otellogr/logsink.go

--- bridges/otellogr/logsink.go
+++ bridges/otellogr/logsink.go
@@ -61,7 +61,7 @@ import (
 
 	"go.opentelemetry.io/otel/log"
 	"go.opentelemetry.io/otel/log/global"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.27.0"
 )
 
 type config struct {
