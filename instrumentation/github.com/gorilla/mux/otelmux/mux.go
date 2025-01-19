a82a7eb112157cb0606349e5533744a26f874cd7
martinpasaribu <martin.yonathan305@gmail.com>
2025-01-20 01:38:39 +0700

    migrate to semconv for otelmux #6638

--- instrumentation/github.com/gorilla/mux/otelmux/mux.go
+++ instrumentation/github.com/gorilla/mux/otelmux/mux.go
@@ -6,15 +6,16 @@ package otelmux // import "go.opentelemetry.io/contrib/instrumentation/github.co
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	"time"
 
-	"github.com/felixge/httpsnoop"
 	"github.com/gorilla/mux"
 
-	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux/internal/semconvutil"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux/internal/semconv"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux/request"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/propagation"
-	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -45,72 +46,42 @@ func Middleware(service string, opts ...Option) mux.MiddlewareFunc {
 		cfg.spanNameFormatter = defaultSpanNameFunc
 	}
 
+	if cfg.MeterProvider == nil {
+		cfg.MeterProvider = otel.GetMeterProvider()
+	}
+
+	cfg.Meter = cfg.MeterProvider.Meter(
+		ScopeName,
+		metric.WithInstrumentationVersion(Version()),
+	)
+
 	return func(handler http.Handler) http.Handler {
 		return traceware{
-			service:           service,
-			tracer:            tracer,
-			propagators:       cfg.Propagators,
-			handler:           handler,
-			spanNameFormatter: cfg.spanNameFormatter,
-			publicEndpoint:    cfg.PublicEndpoint,
-			publicEndpointFn:  cfg.PublicEndpointFn,
-			filters:           cfg.Filters,
+			service:            service,
+			tracer:             tracer,
+			propagators:        cfg.Propagators,
+			handler:            handler,
+			spanNameFormatter:  cfg.spanNameFormatter,
+			publicEndpoint:     cfg.PublicEndpoint,
+			publicEndpointFn:   cfg.PublicEndpointFn,
+			filters:            cfg.Filters,
+			metricAttributesFn: cfg.MetricAttributesFn,
+			semconv:            semconv.NewHTTPServer(cfg.Meter),
 		}
 	}
 }
 
 type traceware struct {
-	service           string
-	tracer            trace.Tracer
-	propagators       propagation.TextMapPropagator
-	handler           http.Handler
-	spanNameFormatter func(string, *http.Request) string
-	publicEndpoint    bool
-	publicEndpointFn  func(*http.Request) bool
-	filters           []Filter
-}
-
-type recordingResponseWriter struct {
-	writer  http.ResponseWriter
-	written bool
-	status  int
-}
-
-var rrwPool = &sync.Pool{
-	New: func() interface{} {
-		return &recordingResponseWriter{}
-	},
-}
-
-func getRRW(writer http.ResponseWriter) *recordingResponseWriter {
-	rrw := rrwPool.Get().(*recordingResponseWriter)
-	rrw.written = false
-	rrw.status = http.StatusOK
-	rrw.writer = httpsnoop.Wrap(writer, httpsnoop.Hooks{
-		Write: func(next httpsnoop.WriteFunc) httpsnoop.WriteFunc {
-			return func(b []byte) (int, error) {
-				if !rrw.written {
-					rrw.written = true
-				}
-				return next(b)
-			}
-		},
-		WriteHeader: func(next httpsnoop.WriteHeaderFunc) httpsnoop.WriteHeaderFunc {
-			return func(statusCode int) {
-				if !rrw.written {
-					rrw.written = true
-					rrw.status = statusCode
-				}
-				next(statusCode)
-			}
-		},
-	})
-	return rrw
-}
-
-func putRRW(rrw *recordingResponseWriter) {
-	rrw.writer = nil
-	rrwPool.Put(rrw)
+	service            string
+	tracer             trace.Tracer
+	propagators        propagation.TextMapPropagator
+	handler            http.Handler
+	spanNameFormatter  func(string, *http.Request) string
+	publicEndpoint     bool
+	publicEndpointFn   func(*http.Request) bool
+	filters            []Filter
+	metricAttributesFn func(*http.Request) []attribute.KeyValue
+	semconv            semconv.HTTPServer
 }
 
 // defaultSpanNameFunc just reuses the route name as the span name.
@@ -119,6 +90,7 @@ func defaultSpanNameFunc(routeName string, _ *http.Request) string { return rout
 // ServeHTTP implements the http.Handler interface. It does the actual
 // tracing of the request.
 func (tw traceware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	requestStartTime := time.Now()
 	for _, f := range tw.filters {
 		if !f(r) {
 			// Simply pass through to the handler if a filter rejects the request
@@ -128,22 +100,8 @@ func (tw traceware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ctx := tw.propagators.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
-	routeStr := ""
-	route := mux.CurrentRoute(r)
-	if route != nil {
-		var err error
-		routeStr, err = route.GetPathTemplate()
-		if err != nil {
-			routeStr, err = route.GetPathRegexp()
-			if err != nil {
-				routeStr = ""
-			}
-		}
-	}
-
 	opts := []trace.SpanStartOption{
-		trace.WithAttributes(semconvutil.HTTPServerRequest(tw.service, r)...),
-		trace.WithSpanKind(trace.SpanKindServer),
+		trace.WithAttributes(tw.semconv.RequestTraceAttrs(tw.service, r)...),
 	}
 
 	if tw.publicEndpoint || (tw.publicEndpointFn != nil && tw.publicEndpointFn(r.WithContext(ctx))) {
@@ -154,21 +112,78 @@ func (tw traceware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	if routeStr == "" {
-		routeStr = fmt.Sprintf("HTTP %s route not found", r.Method)
-	} else {
-		rAttr := semconv.HTTPRoute(routeStr)
+	routeStr := fmt.Sprintf("HTTP %s route not found", r.Method)
+	if route := mux.CurrentRoute(r); route != nil {
+		if pathTemplate, err := route.GetPathTemplate(); err == nil {
+			routeStr = pathTemplate
+		} else if pathRegexp, err := route.GetPathRegexp(); err == nil {
+			routeStr = pathRegexp
+		}
+	}
+
+	if routeStr != fmt.Sprintf("HTTP %s route not found", r.Method) {
+		rAttr := tw.semconv.Route(routeStr)
 		opts = append(opts, trace.WithAttributes(rAttr))
 	}
-	spanName := tw.spanNameFormatter(routeStr, r)
-	ctx, span := tw.tracer.Start(ctx, spanName, opts...)
+
+	ctx, span := tw.tracer.Start(ctx, tw.spanNameFormatter(routeStr, r), opts...)
 	defer span.End()
-	r2 := r.WithContext(ctx)
-	rrw := getRRW(w)
-	defer putRRW(rrw)
-	tw.handler.ServeHTTP(rrw.writer, r2)
-	if rrw.status > 0 {
-		span.SetAttributes(semconv.HTTPStatusCode(rrw.status))
+
+	var readRecordFunc, writeRecordFunc func(int64)
+
+	// if request body is nil or NoBody, we don't want to mutate the body as it
+	// will affect the identity of it in an unforeseeable way because we assert
+	// ReadCloser fulfills a certain interface and it is indeed nil or NoBody.
+	bw := request.NewBodyWrapper(r.Body, readRecordFunc)
+	if r.Body != nil && r.Body != http.NoBody {
+		r.Body = bw
 	}
-	span.SetStatus(semconvutil.HTTPServerStatus(rrw.status))
+
+	rrw := request.NewRespWriterWrapper(w, writeRecordFunc)
+
+	tw.handler.ServeHTTP(w, r.WithContext(ctx))
+	bytesWritten := rrw.BytesWritten()
+	statusCode := rrw.StatusCode()
+
+	if statusCode > 0 {
+		span.SetAttributes(semconv.HTTPStatusCode(statusCode))
+	}
+
+	errCode, errDesc := tw.semconv.Status(statusCode)
+	span.SetStatus(errCode, errDesc)
+	span.SetAttributes(tw.semconv.ResponseTraceAttrs(semconv.ResponseTelemetry{
+		StatusCode: statusCode,
+		ReadBytes:  bw.BytesRead(),
+		ReadError:  bw.Error(),
+		WriteBytes: bytesWritten,
+		WriteError: rrw.Error(),
+	})...)
+
+	// Use floating point division here for higher precision (instead of Millisecond method).
+	elapsedTime := float64(time.Since(requestStartTime)) / float64(time.Millisecond)
+
+	var attrs []attribute.KeyValue
+	metricAttributes := semconv.MetricAttributes{
+		Req:                  r,
+		StatusCode:           statusCode,
+		AdditionalAttributes: append(attrs, tw.metricAttributesFromRequest(r)...),
+	}
+
+	tw.semconv.RecordMetrics(ctx, semconv.ServerMetricData{
+		ServerName:       tw.service,
+		ResponseSize:     bytesWritten,
+		MetricAttributes: metricAttributes,
+		MetricData: semconv.MetricData{
+			RequestSize: bw.BytesRead(),
+			ElapsedTime: elapsedTime,
+		},
+	})
+}
+
+func (tw traceware) metricAttributesFromRequest(r *http.Request) []attribute.KeyValue {
+	var attributeForRequest []attribute.KeyValue
+	if tw.metricAttributesFn != nil {
+		attributeForRequest = tw.metricAttributesFn(r)
+	}
+	return attributeForRequest
 }
