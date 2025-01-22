92636cddf788bf260e130414f0b28b8bf3d5b890
TanishqPorwar <tanishqporwar22@gmail.com>
2025-01-22 20:10:23 +0530

    refactor + add metric support

--- instrumentation/github.com/gorilla/mux/otelmux/mux.go
+++ instrumentation/github.com/gorilla/mux/otelmux/mux.go
@@ -6,6 +6,9 @@ package otelmux // import "go.opentelemetry.io/contrib/instrumentation/github.co
 import (
 	"net/http"
 
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/propagation"
+
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux/internal/semconvutil"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
@@ -33,6 +36,7 @@ func Middleware(service string, opts ...Option) mux.MiddlewareFunc {
 	if cfg.TracerProvider == nil {
 		cfg.TracerProvider = otel.GetTracerProvider()
 	}
+	// TODO: check if this is necessary
 	_ = cfg.TracerProvider.Tracer(
 		ScopeName,
 		trace.WithInstrumentationVersion(Version()),
@@ -42,61 +46,39 @@ func Middleware(service string, opts ...Option) mux.MiddlewareFunc {
 	}
 	if cfg.spanNameFormatter == nil {
 		cfg.spanNameFormatter = defaultSpanNameFunc
-	} else {
-		tmp := cfg.spanNameFormatter
-		cfg.spanNameFormatter = func(op string, r *http.Request) string {
-			routeStr := ""
-			route := mux.CurrentRoute(r)
-			if route != nil {
-				var err error
-				routeStr, err = route.GetPathTemplate()
-				if err != nil {
-					routeStr, err = route.GetPathRegexp()
-					if err != nil {
-						routeStr = op
-					}
-				}
-			}
-			return tmp(routeStr, r)
-		}
 	}
-
-	otelOpts := []otelhttp.Option{
-		otelhttp.WithTracerProvider(cfg.TracerProvider),
-		otelhttp.WithPropagators(cfg.Propagators),
-		otelhttp.WithSpanNameFormatter(cfg.spanNameFormatter),
-		otelhttp.WithPublicEndpointFn(cfg.PublicEndpointFn),
-		otelhttp.WithSpanOptions(),
-	}
-	if cfg.PublicEndpoint {
-		otelOpts = append(otelOpts, otelhttp.WithPublicEndpoint())
-	}
-
-	for _, f := range cfg.Filters {
-		otelOpts = append(otelOpts, otelhttp.WithFilter(otelhttp.Filter(f)))
+	if cfg.meterProvider == nil {
+		cfg.meterProvider = otel.GetMeterProvider()
 	}
 
 	return func(handler http.Handler) http.Handler {
 		return middleware{
-			service:  service,
-			handler:  handler,
-			otelOpts: otelOpts,
+			service:           service,
+			handler:           handler,
+			spanNameFormatter: cfg.spanNameFormatter,
+			tracerProvider:    cfg.TracerProvider,
+			meterProvider:     cfg.meterProvider,
+			propagators:       cfg.Propagators,
+			publicEndpointFn:  cfg.PublicEndpointFn,
+			publicEndpoint:    cfg.PublicEndpoint,
+			filters:           cfg.Filters,
 		}
 	}
 }
 
 type middleware struct {
-	handler  http.Handler
-	service  string
-	otelOpts []otelhttp.Option
+	handler           http.Handler
+	service           string
+	spanNameFormatter func(string, *http.Request) string
+	tracerProvider    trace.TracerProvider
+	propagators       propagation.TextMapPropagator
+	publicEndpointFn  func(*http.Request) bool
+	publicEndpoint    bool
+	filters           []Filter
+	meterProvider     metric.MeterProvider
 }
 
 func (m middleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	traceOpts := []trace.SpanStartOption{
-		trace.WithAttributes(semconvutil.HTTPServerRequest(m.service, request)...),
-		trace.WithSpanKind(trace.SpanKindServer),
-	}
-
 	routeStr := ""
 	route := mux.CurrentRoute(request)
 	if route != nil {
@@ -110,16 +92,38 @@ func (m middleware) ServeHTTP(writer http.ResponseWriter, request *http.Request)
 		}
 	}
 
+	otelOpts := []otelhttp.Option{
+		otelhttp.WithTracerProvider(m.tracerProvider),
+		otelhttp.WithMeterProvider(m.meterProvider),
+		otelhttp.WithPropagators(m.propagators),
+		otelhttp.WithPublicEndpointFn(m.publicEndpointFn),
+		otelhttp.WithSpanNameFormatter(func(op string, r *http.Request) string {
+			return m.spanNameFormatter(routeStr, r)
+		}),
+	}
+
+	if m.publicEndpoint {
+		otelOpts = append(otelOpts, otelhttp.WithPublicEndpoint())
+	}
+
+	for _, f := range m.filters {
+		otelOpts = append(otelOpts, otelhttp.WithFilter(otelhttp.Filter(f)))
+	}
+	traceOpts := []trace.SpanStartOption{
+		trace.WithAttributes(semconvutil.HTTPServerRequest(m.service, request)...),
+		trace.WithSpanKind(trace.SpanKindServer),
+	}
+
 	if routeStr != "" {
 		rAttr := semconv.HTTPRoute(routeStr)
 		traceOpts = append(traceOpts, trace.WithAttributes(rAttr))
 	}
-	m.otelOpts = append(m.otelOpts, otelhttp.WithSpanOptions(traceOpts...))
+	otelOpts = append(otelOpts, otelhttp.WithSpanOptions(traceOpts...))
 
 	h := otelhttp.NewHandler(
 		m.handler,
 		m.service,
-		m.otelOpts...,
+		otelOpts...,
 	)
 	h.ServeHTTP(writer, request)
 }
@@ -128,17 +132,5 @@ var _ http.Handler = middleware{}
 
 // defaultSpanNameFunc just reuses the route name as the span name.
 func defaultSpanNameFunc(routeName string, r *http.Request) string {
-	routeStr := routeName
-	route := mux.CurrentRoute(r)
-	if route != nil {
-		var err error
-		routeStr, err = route.GetPathTemplate()
-		if err != nil {
-			routeStr, err = route.GetPathRegexp()
-			if err != nil {
-				routeStr = ""
-			}
-		}
-	}
-	return routeStr
+	return routeName
 }
