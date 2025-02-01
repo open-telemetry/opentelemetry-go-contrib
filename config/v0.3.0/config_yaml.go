8e255d10605d34d16489c5687a5b6a1388b467c3
Mike Blum <me+github@mblum.me>
2025-02-01 08:52:20 -0600

    demo: error on empty attributes list

--- config/v0.3.0/config_yaml.go
+++ config/v0.3.0/config_yaml.go
@@ -7,8 +7,26 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
-	"strings"
 )
+
+func (c *OpenTelemetryConfiguration) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type alias OpenTelemetryConfiguration // Prevents infinite recursion
+	aux := &alias{}
+
+	// Decode into alias
+	if err := unmarshal(aux); err != nil {
+		return err
+	}
+
+	// Check for an empty attributes list
+	if len(aux.Resource.Attributes) == 0 {
+		return fmt.Errorf("error: 'attributes' list cannot be empty")
+	}
+
+	// Assign parsed data back to actual struct
+	*c = OpenTelemetryConfiguration(*aux)
+	return nil
+}
 
 // UnmarshalYAML implements yaml.Unmarshaler.
 func (j *AttributeNameValueType) UnmarshalYAML(unmarshal func(interface{}) error) error {
@@ -34,53 +52,19 @@ func (j *AttributeNameValueType) UnmarshalYAML(unmarshal func(interface{}) error
 
 // UnmarshalYAML implements yaml.Unmarshaler.
 func (j *AttributeNameValue) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var raw map[string]interface{}
-	if err := unmarshal(&raw); err != nil {
+	type alias AttributeNameValue
+	aux := &alias{}
+
+	if err := unmarshal(aux); err != nil {
 		return err
 	}
-	var name string
-	var value interface{}
-	var ok bool
-	if _, ok = raw["name"]; raw != nil && !ok {
-		return errors.New("field name in AttributeNameValue: required")
-	}
-	if name, ok = raw["name"].(string); !ok {
-		return errors.New("yaml: cannot unmarshal field name in AttributeNameValue must be string")
-	}
-	if strings.TrimSpace(name) == "" {
-		return errors.New("field name in AttributeNameValue: required")
-	}
-	if value, ok = raw["value"]; raw != nil && !ok {
-		return errors.New("field value in AttributeNameValue: required")
-	}
-	type Plain AttributeNameValue
-	plain := Plain{
-		Name:  name,
-		Value: value,
-	}
-	if plain.Type != nil && plain.Type.Value == "int" {
-		val, ok := plain.Value.(float64)
-		if ok {
-			plain.Value = int(val)
-		}
-	}
-	if plain.Type != nil && plain.Type.Value == "int_array" {
-		m, ok := plain.Value.([]interface{})
-		if ok {
-			var vals []interface{}
-			for _, v := range m {
-				val, ok := v.(float64)
-				if ok {
-					vals = append(vals, int(val))
-				} else {
-					vals = append(vals, val)
-				}
-			}
-			plain.Value = vals
-		}
+
+	// Check for empty name
+	if aux.Name == "" {
+		return fmt.Errorf("error: attribute 'name' cannot be empty")
 	}
 
-	*j = AttributeNameValue(plain)
+	*j = AttributeNameValue(*aux)
 	return nil
 }
 
