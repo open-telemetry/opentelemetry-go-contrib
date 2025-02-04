8367e61a7ca413acd143b78b15d4fe73f3908806
dmathieu <42@dmathieu.com>
2025-02-04 11:04:54 +0100

    generate semconv package for otelrestful

---
+++ instrumentation/github.com/emicklei/go-restful/otelrestful/internal/semconv/env_test.go
@@ -0,0 +1,134 @@
+// Code created by gotmpl. DO NOT MODIFY.
+// source: internal/shared/semconv/env_test.go.tmpl
+
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package semconv
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/otel/metric/noop"
+)
+
+func TestHTTPServerDoesNotPanic(t *testing.T) {
+	testCases := []struct {
+		name   string
+		server HTTPServer
+	}{
+		{
+			name:   "empty",
+			server: HTTPServer{},
+		},
+		{
+			name:   "nil meter",
+			server: NewHTTPServer(nil),
+		},
+		{
+			name:   "with Meter",
+			server: NewHTTPServer(noop.Meter{}),
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			require.NotPanics(t, func() {
+				req, err := http.NewRequest("GET", "http://example.com", nil)
+				require.NoError(t, err)
+
+				_ = tt.server.RequestTraceAttrs("stuff", req)
+				_ = tt.server.ResponseTraceAttrs(ResponseTelemetry{StatusCode: 200})
+				tt.server.RecordMetrics(context.Background(), ServerMetricData{
+					ServerName: "stuff",
+					MetricAttributes: MetricAttributes{
+						Req: req,
+					},
+				})
+			})
+		})
+	}
+}
+
+func TestHTTPClientDoesNotPanic(t *testing.T) {
+	testCases := []struct {
+		name   string
+		client HTTPClient
+	}{
+		{
+			name:   "empty",
+			client: HTTPClient{},
+		},
+		{
+			name:   "nil meter",
+			client: NewHTTPClient(nil),
+		},
+		{
+			name:   "with Meter",
+			client: NewHTTPClient(noop.Meter{}),
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			require.NotPanics(t, func() {
+				req, err := http.NewRequest("GET", "http://example.com", nil)
+				require.NoError(t, err)
+
+				_ = tt.client.RequestTraceAttrs(req)
+				_ = tt.client.ResponseTraceAttrs(&http.Response{StatusCode: 200})
+
+				opts := tt.client.MetricOptions(MetricAttributes{
+					Req:        req,
+					StatusCode: 200,
+				})
+				tt.client.RecordResponseSize(context.Background(), 40, opts)
+				tt.client.RecordMetrics(context.Background(), MetricData{
+					RequestSize: 20,
+					ElapsedTime: 1,
+				}, opts)
+			})
+		})
+	}
+}
+
+func BenchmarkRecordMetrics(b *testing.B) {
+	benchmarks := []struct {
+		name   string
+		server HTTPServer
+	}{
+		{
+			name:   "empty",
+			server: HTTPServer{},
+		},
+		{
+			name:   "nil meter",
+			server: NewHTTPServer(nil),
+		},
+		{
+			name:   "with Meter",
+			server: NewHTTPServer(noop.Meter{}),
+		},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			req, _ := http.NewRequest("GET", "http://example.com", nil)
+			_ = bm.server.RequestTraceAttrs("stuff", req)
+			_ = bm.server.ResponseTraceAttrs(ResponseTelemetry{StatusCode: 200})
+			ctx := context.Background()
+			b.ReportAllocs()
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				bm.server.RecordMetrics(ctx, ServerMetricData{
+					ServerName: bm.name,
+					MetricAttributes: MetricAttributes{
+						Req: req,
+					},
+				})
+			}
+		})
+	}
+}
