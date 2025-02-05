5029ece097bc45503d97e12a2bfd3688ab056af2
dmathieu <42@dmathieu.com>
2025-02-05 12:44:09 +0100

    add tests for the new semconv methods

--- instrumentation/net/http/otelhttp/internal/semconv/env_test.go
+++ instrumentation/net/http/otelhttp/internal/semconv/env_test.go
@@ -55,6 +55,51 @@ func TestHTTPServerDoesNotPanic(t *testing.T) {
 	}
 }
 
+func TestServerNetworkTransportAttr(t *testing.T) {
+	for _, tt := range []struct {
+		name     string
+		optinVal string
+		network  string
+
+		wantAttributes []attribute.KeyValue
+	}{
+		{
+			name:    "without any opt-in",
+			network: "tcp",
+
+			wantAttributes: []attribute.KeyValue{
+				attribute.String("net.transport", "ip_tcp"),
+			},
+		},
+		{
+			name:     "without an old optin",
+			optinVal: "old",
+			network:  "tcp",
+
+			wantAttributes: []attribute.KeyValue{
+				attribute.String("net.transport", "ip_tcp"),
+			},
+		},
+		{
+			name:     "without a dup optin",
+			optinVal: "http/dup",
+			network:  "tcp",
+
+			wantAttributes: []attribute.KeyValue{
+				attribute.String("net.transport", "ip_tcp"),
+				attribute.String("network.transport", "tcp"),
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(OTelSemConvStabilityOptIn, tt.optinVal)
+			s := NewHTTPServer(nil)
+
+			assert.Equal(t, tt.wantAttributes, s.NetworkTransportAttr(tt.network))
+		})
+	}
+}
+
 func TestHTTPClientDoesNotPanic(t *testing.T) {
 	testCases := []struct {
 		name   string
@@ -134,6 +179,51 @@ func TestHTTPClientTraceAttributes(t *testing.T) {
 			c := NewHTTPClient(nil)
 			a := c.TraceAttributes("example.com")
 			assert.Equal(t, tt.wantAttributes, a)
+		})
+	}
+}
+
+func TestClientTraceAttributes(t *testing.T) {
+	for _, tt := range []struct {
+		name     string
+		optinVal string
+		host     string
+
+		wantAttributes []attribute.KeyValue
+	}{
+		{
+			name: "without any opt-in",
+			host: "example.com",
+
+			wantAttributes: []attribute.KeyValue{
+				attribute.String("net.host.name", "example.com"),
+			},
+		},
+		{
+			name:     "without an old optin",
+			optinVal: "old",
+			host:     "example.com",
+
+			wantAttributes: []attribute.KeyValue{
+				attribute.String("net.host.name", "example.com"),
+			},
+		},
+		{
+			name:     "without a dup optin",
+			optinVal: "http/dup",
+			host:     "example.com",
+
+			wantAttributes: []attribute.KeyValue{
+				attribute.String("net.host.name", "example.com"),
+				attribute.String("server.address", "example.com"),
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(OTelSemConvStabilityOptIn, tt.optinVal)
+			s := NewHTTPClient(nil)
+
+			assert.Equal(t, tt.wantAttributes, s.TraceAttributes(tt.host))
 		})
 	}
 }
