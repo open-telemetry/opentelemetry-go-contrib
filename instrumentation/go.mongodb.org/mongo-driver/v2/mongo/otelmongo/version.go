959088719b422e2ceae270898edc34cc8b8321ce
Alaric Whitney <alaric.whitney@gmail.com>
2025-02-14 20:26:47 -0600

    Update instrumentation/go.mongodb.org/mongo-driver/v2/mongo/otelmongo/version.go
    Co-authored-by: Preston Vasquez <prestonvasquez@icloud.com>

--- instrumentation/go.mongodb.org/mongo-driver/v2/mongo/otelmongo/version.go
+++ instrumentation/go.mongodb.org/mongo-driver/v2/mongo/otelmongo/version.go
@@ -3,7 +3,7 @@
 
 package otelmongo // import "go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/v2/mongo/otelmongo"
 
-// Version is the current release version of the mongo-driver instrumentation.
+// Version is the current release version of the mongo-go-driver V2 instrumentation.
 func Version() string {
 	return "0.58.0"
 	// This string is updated by the pre_release.sh script during release
