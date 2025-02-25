1c28d9d2667fb66433759fec92073ab4633868a4
Robert Pajak <pellared@hotmail.com>
2025-02-25 10:27:03 +0100

    logsdk "go.opentelemetry.io/otel/sdk/log"

--- processors/minsev/example_test.go
+++ processors/minsev/example_test.go
@@ -11,19 +11,19 @@ import (
 	"sync"
 
 	"go.opentelemetry.io/contrib/processors/minsev"
-	logapi "go.opentelemetry.io/otel/log"
-	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/log"
+	logsdk "go.opentelemetry.io/otel/sdk/log"
 )
 
 const key = "OTEL_LOG_LEVEL"
 
-var getSeverity = sync.OnceValue(func() logapi.Severity {
-	conv := map[string]logapi.Severity{
-		"":      logapi.SeverityInfo, // Default to SeverityInfo for unset.
-		"debug": logapi.SeverityDebug,
-		"info":  logapi.SeverityInfo,
-		"warn":  logapi.SeverityWarn,
-		"error": logapi.SeverityError,
+var getSeverity = sync.OnceValue(func() log.Severity {
+	conv := map[string]log.Severity{
+		"":      log.SeverityInfo, // Default to SeverityInfo for unset.
+		"debug": log.SeverityDebug,
+		"info":  log.SeverityInfo,
+		"warn":  log.SeverityWarn,
+		"error": log.SeverityError,
 	}
 	// log.SeverityUndefined for unknown values.
 	return conv[strings.ToLower(os.Getenv(key))]
@@ -31,30 +31,30 @@ var getSeverity = sync.OnceValue(func() logapi.Severity {
 
 type EnvSeverity struct{}
 
-func (EnvSeverity) Severity() logapi.Severity { return getSeverity() }
+func (EnvSeverity) Severity() log.Severity { return getSeverity() }
 
 func ExampleSeveritier() {
 	// Mock an environmental variable setup that would be done externally.
 	_ = os.Setenv(key, "error")
 
 	// Existing processor that emits telemetry.
-	var processor log.Processor = log.NewBatchProcessor(nil)
+	var processor logsdk.Processor = logsdk.NewBatchProcessor(nil)
 
 	// Wrap the processor so that it filters by severity level defined
 	// via environmental variable.
 	processor = minsev.NewLogProcessor(processor, EnvSeverity{})
-	lp := log.NewLoggerProvider(
-		log.WithProcessor(processor),
+	lp := logsdk.NewLoggerProvider(
+		logsdk.WithProcessor(processor),
 	)
 
 	// Show that Logs API respects the minimum severity level processor.
 	l := lp.Logger("ExampleSeveritier")
 
 	ctx := context.Background()
-	params := logapi.EnabledParameters{Severity: logapi.SeverityDebug}
+	params := log.EnabledParameters{Severity: log.SeverityDebug}
 	fmt.Println(l.Enabled(ctx, params))
 
-	params.Severity = logapi.SeverityError
+	params.Severity = log.SeverityError
 	fmt.Println(l.Enabled(ctx, params))
 
 	// Output:
