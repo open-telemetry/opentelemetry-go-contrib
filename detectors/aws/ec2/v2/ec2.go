736816a544cb00f527c35be21f820adb60699269
Alex Kats <akats71@gmail.com>
2025-03-27 23:09:01 -0400

    update version

--- detectors/aws/ec2/v2/ec2.go
+++ detectors/aws/ec2/v2/ec2.go
@@ -17,7 +17,7 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.30.0"
 )
 
 var errClient = errors.New("EC2 Client Error")
