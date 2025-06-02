bafe9f4768da40eb4d9bb64d31926975066943b6
Jan Steinke <me@janxyz.de>
2025-06-02 10:34:09 +0200

    clean up imports

--- instrumentation/google.golang.org/grpc/otelgrpc/stats_handler_test.go
+++ instrumentation/google.golang.org/grpc/otelgrpc/stats_handler_test.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/stats"
 
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/embedded"
@@ -17,8 +19,6 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	"go.opentelemetry.io/otel/trace"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/stats"
 )
 
 func TestWithPublicEndpoint(t *testing.T) {
