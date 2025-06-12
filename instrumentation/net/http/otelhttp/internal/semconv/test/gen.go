80d8c366d6e5532b414fb3c2392204519631a489
Robert Pająk <pellared@hotmail.com>
2025-06-12 19:06:58 +0200

    Remove semconv/test modules (#7445)

--- instrumentation/net/http/otelhttp/internal/semconv/test/gen.go
+++
@@ -1,9 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-// Package test provides semantic convention tests for otelhttp.
-package test // import "go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp/internal/semconv/test"
-
-// Generate semconv/test package:
-//go:generate gotmpl --body=../../../../../../../internal/shared/semconv/test/common_test.go.tmpl "--data={ \"pkg\": \"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp\" }" --out=common_test.go
-//go:generate gotmpl --body=../../../../../../../internal/shared/semconv/test/httpconv_test.go.tmpl "--data={ \"pkg\": \"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp\" }" --out=httpconv_test.go
