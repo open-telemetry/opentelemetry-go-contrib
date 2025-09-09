d0061c785689c5899642bda667401b22f402c87f
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-09-09 18:38:47 +0200

    Add mutex field to IDGenerator struct

--- propagators/aws/xray/idgenerator.go
+++ propagators/aws/xray/idgenerator.go
@@ -20,6 +20,7 @@ import (
 // IDGenerator is used for generating a new traceID and spanID.
 type IDGenerator struct {
 	sync.Mutex // Deprecated: mutex is internal and shall not be used.
+	mu         sync.Mutex
 	randSource *rand.Rand
 }
 
