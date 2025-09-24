b5d68465ae8f252c645673c181be33ccb60cb74e
alex boten <223565+codeboten@users.noreply.github.com>
2025-09-24 13:49:26 -0700

    add invalid map case
    Signed-off-by: alex boten <223565+codeboten@users.noreply.github.com>

--- otelconf/v0.3.0/config_test.go
+++ otelconf/v0.3.0/config_test.go
@@ -411,7 +411,7 @@ var v03OpenTelemetryConfigEnvParsing = OpenTelemetryConfiguration{
 			{Name: "quoted_float_value", Type: &AttributeNameValueType{Value: "string"}, Value: "1.1"},
 			{Name: "quoted_hex_value", Type: &AttributeNameValueType{Value: "string"}, Value: "0xbeef"},
 			{Name: "alternative_env_syntax", Type: &AttributeNameValueType{Value: "string"}, Value: "value"},
-			// {Name: "invalid_map_value", Type: &AttributeNameValueType{Value: "string"}, Value: "value"},
+			{Name: "invalid_map_value", Type: &AttributeNameValueType{Value: "string"}, Value: "value\nkey:value"},
 			{Name: "multiple_references_inject", Type: &AttributeNameValueType{Value: "string"}, Value: "foo value 1.1"},
 			{Name: "undefined_key", Type: &AttributeNameValueType{Value: "string"}, Value: nil},
 			{Name: "undefined_key_fallback", Type: &AttributeNameValueType{Value: "string"}, Value: "fallback"},
@@ -528,7 +528,7 @@ func TestParseYAMLWithEnvironmentVariables(t *testing.T) {
 	t.Setenv("INT_VALUE", "1")
 	t.Setenv("FLOAT_VALUE", "1.1")
 	t.Setenv("HEX_VALUE", "0xbeef")                       // A valid integer value (i.e. 3735928559) written in hexadecimal
-	t.Setenv("INVALID_MAP_VALUE", "value\nkey:value")     // An invalid attempt to inject a map key into the YAML
+	t.Setenv("INVALID_MAP_VALUE", "value\\nkey:value")    // An invalid attempt to inject a map key into the YAML
 	t.Setenv("DO_NOT_REPLACE_ME", "Never use this value") // An unused environment variable
 	t.Setenv("REPLACE_ME", "${DO_NOT_REPLACE_ME}")        // A valid replacement text, used verbatim, not replaced with "Never use this value"
 	t.Setenv("VALUE_WITH_ESCAPE", "value$$")              // A valid replacement text, used verbatim, not replaced with "Never use this value"
