2a2eb68c89006659526facbde7037322c3d28b93
alex boten <223565+codeboten@users.noreply.github.com>
2025-09-25 12:03:23 -0700

    copy v0.3.0 and update import definition
    Signed-off-by: alex boten <223565+codeboten@users.noreply.github.com>

--- otelconf/config_yaml.go
+++ otelconf/config_yaml.go
@@ -5,6 +5,8 @@ package otelconf // import "go.opentelemetry.io/contrib/otelconf"
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
 
 	"go.yaml.in/yaml/v3"
 )
@@ -36,6 +38,28 @@ func (j *PushMetricExporter) UnmarshalYAML(node *yaml.Node) error {
 		plain.Console = ConsoleExporter{}
 	}
 	*j = PushMetricExporter(plain)
+	return nil
+}
+
+// UnmarshalYAML implements yaml.Unmarshaler.
+func (j *AttributeNameValueType) UnmarshalYAML(unmarshal func(any) error) error {
+	var v struct {
+		Value any
+	}
+	if err := unmarshal(&v.Value); err != nil {
+		return err
+	}
+	var ok bool
+	for _, expected := range enumValuesAttributeNameValueType {
+		if reflect.DeepEqual(v.Value, expected) {
+			ok = true
+			break
+		}
+	}
+	if !ok {
+		return fmt.Errorf("invalid value (expected one of %#v): %#v", enumValuesAttributeNameValueType, v.Value)
+	}
+	*j = AttributeNameValueType(v)
 	return nil
 }
 
@@ -120,6 +144,31 @@ func (j *BatchLogRecordProcessor) UnmarshalYAML(node *yaml.Node) error {
 	*j = BatchLogRecordProcessor(plain)
 	return nil
 }
+func (j *NameStringValuePair) UnmarshalYAML(unmarshal func(any) error) error {
+	var raw map[string]any
+	if err := unmarshal(&raw); err != nil {
+		return err
+	}
+	if _, ok := raw["name"]; !ok {
+		return errors.New("yaml: cannot unmarshal field name in NameStringValuePair required")
+	}
+	if _, ok := raw["value"]; !ok {
+		return errors.New("yaml: cannot unmarshal field value in NameStringValuePair required")
+	}
+	var name, value string
+	var ok bool
+	if name, ok = raw["name"].(string); !ok {
+		return errors.New("yaml: cannot unmarshal field name in NameStringValuePair must be string")
+	}
+	if value, ok = raw["value"].(string); !ok {
+		return errors.New("yaml: cannot unmarshal field value in NameStringValuePair must be string")
+	}
+	*j = NameStringValuePair{
+		Name:  name,
+		Value: &value,
+	}
+	return nil
+}
 
 // UnmarshalYAML implements yaml.Unmarshaler.
 func (j *BatchSpanProcessor) UnmarshalYAML(node *yaml.Node) error {
@@ -180,5 +229,14 @@ func (j *SpanLimits) UnmarshalYAML(node *yaml.Node) error {
 		return err
 	}
 	*j = SpanLimits(plain)
+	return nil
+}
+func (j *LanguageSpecificInstrumentation) UnmarshalYAML(unmarshal func(any) error) error {
+	var raw map[string]any
+	if err := unmarshal(&raw); err != nil {
+		return err
+	}
+
+	*j = raw
 	return nil
 }
