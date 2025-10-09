d9df9a77dd17381b537ae29e8228e109c5d8db4b
Alex Boten <223565+codeboten@users.noreply.github.com>
2025-10-09 07:04:18 -0700

    Update otelconf/internal/provider/envprovider_test.go
    Co-authored-by: Flc゛ <i@flc.io>

--- otelconf/internal/provider/envprovider_test.go
+++ otelconf/internal/provider/envprovider_test.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package provider // import "go.opentelemetry.io/contrib/internal/provider"
+package provider // import "go.opentelemetry.io/contrib/otelconf/internal/provider"
 
 import (
 	"errors"
