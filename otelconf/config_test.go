6237c08a8239ad0ad72587f540b2ac550cf30e68
alex boten <223565+codeboten@users.noreply.github.com>
2025-10-23 13:10:39 -0700

    remove createTLSConfig, reuse refactored code
    Signed-off-by: alex boten <223565+codeboten@users.noreply.github.com>

--- otelconf/config_test.go
+++ otelconf/config_test.go
@@ -4,7 +4,6 @@
 package otelconf
 
 import (
-	"crypto/tls"
 	"encoding/json"
 	"errors"
 	"os"
@@ -1334,61 +1333,6 @@ func TestUnmarshalPeriodicMetricReader(t *testing.T) {
 			err = yaml.Unmarshal(tt.yamlConfig, &pmr)
 			assert.ErrorIs(t, err, tt.wantErrT)
 			assert.Equal(t, tt.wantExporter, pmr.Exporter)
-		})
-	}
-}
-func TestCreateTLSConfig(t *testing.T) {
-	tests := []struct {
-		name            string
-		caCertFile      *string
-		clientCertFile  *string
-		clientKeyFile   *string
-		wantErrContains string
-		want            func(*tls.Config, *testing.T)
-	}{
-		{
-			name: "no-input",
-			want: func(result *tls.Config, t *testing.T) {
-				require.Nil(t, result.Certificates)
-				require.Nil(t, result.RootCAs)
-			},
-		},
-		{
-			name:       "only-cacert-provided",
-			caCertFile: ptr(filepath.Join("..", "testdata", "ca.crt")),
-			want: func(result *tls.Config, t *testing.T) {
-				require.Nil(t, result.Certificates)
-				require.NotNil(t, result.RootCAs)
-			},
-		},
-		{
-			name:            "nonexistent-cacert-file",
-			caCertFile:      ptr("nowhere.crt"),
-			wantErrContains: "open nowhere.crt:",
-		},
-		{
-			name:            "nonexistent-clientcert-file",
-			clientCertFile:  ptr("nowhere.crt"),
-			clientKeyFile:   ptr("nowhere.crt"),
-			wantErrContains: "could not use client certificate: open nowhere.crt:",
-		},
-		{
-			name:            "bad-cacert-file",
-			caCertFile:      ptr(filepath.Join("..", "testdata", "bad_cert.crt")),
-			wantErrContains: "could not create certificate authority chain from certificate",
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := createTLSConfig(tt.caCertFile, tt.clientCertFile, tt.clientKeyFile)
-
-			if tt.wantErrContains != "" {
-				require.Contains(t, err.Error(), tt.wantErrContains)
-			} else {
-				require.NoError(t, err)
-				tt.want(got, t)
-			}
 		})
 	}
 }
