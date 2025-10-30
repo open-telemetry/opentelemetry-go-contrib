39cce82fcf097c41baad160295429a344138ccc7
Alex Boten <223565+codeboten@users.noreply.github.com>
2025-10-30 08:53:19 -0700

    Update otelconf/config_common.go
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- otelconf/config_common.go
+++ otelconf/config_common.go
@@ -238,11 +238,9 @@ func createHeadersConfig(headers []NameStringValuePair, headersList *string) (ma
 		}
 	}
 	// Headers take precedence over HeadersList, so this has to be after HeadersList is processed
-	if len(headers) > 0 {
-		for _, kv := range headers {
-			if kv.Value != nil {
-				result[kv.Name] = *kv.Value
-			}
+	for _, kv := range headers {
+		if kv.Value != nil {
+			result[kv.Name] = *kv.Value
 		}
 	}
 	return result, nil
