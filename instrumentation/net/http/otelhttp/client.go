1e816d80f5f4beb664e3fb5f9bb2c9109d5a0615
KonstantinosTsitoumis <tsitoumis.k@gmail.com>
2025-11-08 18:46:45 +0200

    resolve code review comments

--- instrumentation/net/http/otelhttp/client.go
+++ instrumentation/net/http/otelhttp/client.go
@@ -11,11 +11,13 @@ import (
 	"strings"
 )
 
-// Deprecated: This will be deleted in the future.
-//
 // DefaultClient is the default Client and is used by Get, Head, Post and PostForm.
 // Please be careful of initialization order - for example, if you change
 // the global propagator, the DefaultClient might still be using the old one.
+//
+// Deprecated: DefaultClient will be removed in a future release.
+// Create your own *http.Client with Transport = otelhttp.NewTransport(http.DefaultTransport)
+// instead of relying on this global variable.
 var DefaultClient = &http.Client{Transport: NewTransport(http.DefaultTransport)}
 
 // Get is a convenient replacement for http.Get that adds a span around the request.
