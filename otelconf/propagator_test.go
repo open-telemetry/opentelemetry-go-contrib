38e5cbccee1f14048593a6a5a54174679a1b6ee3
alex boten <223565+codeboten@users.noreply.github.com>
2025-12-15 11:57:37 -0800

    fix imports
    Signed-off-by: alex boten <223565+codeboten@users.noreply.github.com>

--- otelconf/propagator_test.go
+++ otelconf/propagator_test.go
@@ -7,10 +7,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/propagation"
+
 	"go.opentelemetry.io/contrib/propagators/b3"
 	"go.opentelemetry.io/contrib/propagators/jaeger"
 	"go.opentelemetry.io/contrib/propagators/ot"
-	"go.opentelemetry.io/otel/propagation"
 )
 
 func TestPropagator(t *testing.T) {
