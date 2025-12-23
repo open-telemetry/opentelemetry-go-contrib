5d84b8f939996ccefab6eba1e91ef01476bd8338
srivastavya02 <srivastavya24@gmail.com>
2025-12-23 11:31:40 +0530

    gofmt samplers/jaegerremote

--- samplers/jaegerremote/version.go
+++ samplers/jaegerremote/version.go
@@ -6,4 +6,3 @@ package jaegerremote // import "go.opentelemetry.io/contrib/samplers/jaegerremot
 // Version is the current release version of the Jaeger remote sampler.
 // This string is updated by the pre_release.sh script during release.
 const Version = "0.33.0"
-
