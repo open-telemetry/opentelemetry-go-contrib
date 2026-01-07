1fe1f44e82448b03f724714a659a79319fc54694
ELENA ZVEREVA <55851043+ElenaZvereva@users.noreply.github.com>
2026-01-07 20:28:04 +0300

    delete comment from documentation
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- instrumentation/google.golang.org/grpc/otelgrpc/version.go
+++ instrumentation/google.golang.org/grpc/otelgrpc/version.go
@@ -4,5 +4,4 @@
 package otelgrpc // import "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 
 // Version is the current release version of the gRPC instrumentation.
-// It is updated by the pre_release.sh script during release.
 const Version = "0.64.0"
