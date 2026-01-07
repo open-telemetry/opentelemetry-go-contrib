6baa43eea8ad429d87fadb552fed35cc7e772683
ELENA ZVEREVA <55851043+ElenaZvereva@users.noreply.github.com>
2026-01-07 20:30:40 +0300

    delete comment from documentation

--- instrumentation/github.com/gin-gonic/gin/otelgin/version.go
+++ instrumentation/github.com/gin-gonic/gin/otelgin/version.go
@@ -4,5 +4,4 @@
 package otelgin // import "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 
 // Version is the current release version of the gin instrumentation.
-// It is updated by the pre_release.sh script during release.
 const Version = "0.64.0"
