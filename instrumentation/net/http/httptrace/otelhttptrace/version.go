5ec13be68258548991ac56f83c2c370a32ad149e
ELENA ZVEREVA <55851043+ElenaZvereva@users.noreply.github.com>
2026-01-07 20:23:23 +0300

    remove comment from documentation
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- instrumentation/net/http/httptrace/otelhttptrace/version.go
+++ instrumentation/net/http/httptrace/otelhttptrace/version.go
@@ -4,5 +4,4 @@
 package otelhttptrace // import "go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace"
 
 // Version is the current release version of the httptrace instrumentation.
-// It is updated by the pre_release.sh script during release.
 const Version = "0.64.0"
