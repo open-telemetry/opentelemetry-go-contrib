c849a236499f5d247bbb61deebff9bc0a5d0888d
srivastavya02 <srivastavya24@gmail.com>
2026-01-09 17:39:09 +0530

    doc(jaegerremote): move internal comment to hide from pkgsite

--- samplers/jaegerremote/version.go
+++ samplers/jaegerremote/version.go
@@ -3,6 +3,7 @@
 
 package jaegerremote // import "go.opentelemetry.io/contrib/samplers/jaegerremote"
 
-// Version is the current release version of the Jaeger remote sampler.
 // This string is updated by the pre_release.sh script during release.
-const Version = "0.33.0"
+
+// Version is the current release version of the Jaeger remote sampler.
+const Version = "0.33.0" 
