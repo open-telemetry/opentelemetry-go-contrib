d2a8adacf06c26a7342db1ddf45753dc6f565cb0
Damien Mathieu <42@dmathieu.com>
2026-01-12 10:03:42 +0100

    Update instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo/version.go
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo/version.go
+++ instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo/version.go
@@ -4,5 +4,4 @@
 package otelmongo // import "go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
 
 // Version is the current release version of the mongo-driver instrumentation.
-// It is updated by the pre_release.sh script during release.
 const Version = "0.64.0"
