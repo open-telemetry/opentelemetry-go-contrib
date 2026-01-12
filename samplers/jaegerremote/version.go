2f34fdf8f6c7eb9fb949be85ed0db6c5039bc2b7
Damien Mathieu <42@dmathieu.com>
2026-01-12 09:17:47 +0100

    Apply suggestion from @pellared
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- samplers/jaegerremote/version.go
+++ samplers/jaegerremote/version.go
@@ -3,7 +3,6 @@
 
 package jaegerremote // import "go.opentelemetry.io/contrib/samplers/jaegerremote"
 
-// This string is updated by the pre_release.sh script during release.
 
 // Version is the current release version of the Jaeger remote sampler.
 const Version = "0.33.0" 
