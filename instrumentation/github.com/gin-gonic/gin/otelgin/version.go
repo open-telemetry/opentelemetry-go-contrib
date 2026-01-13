748e224b1293dbebb202506e5fbba6b825a10ff4
ELENA ZVEREVA <55851043+ElenaZvereva@users.noreply.github.com>
2026-01-13 14:11:03 +0500

    refactor(otelgin): switch version from function to string (#8341)
    Co-authored-by: Damien Mathieu <42@dmathieu.com>

--- instrumentation/github.com/gin-gonic/gin/otelgin/version.go
+++ instrumentation/github.com/gin-gonic/gin/otelgin/version.go
@@ -4,7 +4,4 @@
 package otelgin // import "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 
 // Version is the current release version of the gin instrumentation.
-func Version() string {
-	return "0.64.0"
-	// This string is updated by the pre_release.sh script during release
-}
+const Version = "0.64.0"
