58113492e0640af3a6c73b55d8dcd181ced44355
rxfa <rafa3rnrn@gmail.com>
2026-01-14 00:17:17 +0000

    chore(otelhttp): remove ReadBytesKey, ReadErrorKey, WroteBytesKey, WriteErrorKey from public API
    Signed-off-by: rxfa <rafa3rnrn@gmail.com>

--- instrumentation/net/http/otelhttp/common.go
+++ instrumentation/net/http/otelhttp/common.go
@@ -6,16 +6,7 @@ package otelhttp // import "go.opentelemetry.io/contrib/instrumentation/net/http
 import (
 	"net/http"
 
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
-)
-
-// Attribute keys that can be added to a span.
-const (
-	ReadBytesKey  = attribute.Key("http.read_bytes")  // if anything was read from the request body, the total number of bytes read
-	ReadErrorKey  = attribute.Key("http.read_error")  // If an error occurred while reading a request, the string of the error (io.EOF is not recorded)
-	WroteBytesKey = attribute.Key("http.wrote_bytes") // if anything was written to the response writer, the total number of bytes written
-	WriteErrorKey = attribute.Key("http.write_error") // if an error occurred while writing a reply, the string of the error (io.EOF is not recorded)
 )
 
 // Filter is a predicate used to determine whether a given http.request should
