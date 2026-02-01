d387510eecf406d31cdd20b7b4cee78326bf46b9
ELENA ZVEREVA <55851043+ElenaZvereva@users.noreply.github.com>
2026-02-01 17:17:45 +0500

    Add version file for otellogrus bridge (#8485)
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

---
+++ bridges/otellogrus/version.go
@@ -0,0 +1,7 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package otellogrus // import "go.opentelemetry.io/contrib/bridges/otellogrus"
+
+// Version is the current release version of the otellogrus bridge.
+const Version = "0.14.0"
