8b6408acb5c06b7ba6172e5fb0867bd75d0b49a7
pransoft7 <pranava.s.kumar@gmail.com>
2026-02-01 18:29:29 +0530

    add version file for autoprop propagator

---
+++ propagators/autoprop/version.go
@@ -0,0 +1,7 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package autoprop // import "go.opentelemetry.io/contrib/propagators/autoprop"
+
+// Version is the current release version of the autoprop module.
+const Version = "0.64.0"
