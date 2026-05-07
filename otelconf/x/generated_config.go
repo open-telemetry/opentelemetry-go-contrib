80c1cd689c7ee4e91a81797c4846bfcac7a381b3
Israel Blancas <iblancasa@gmail.com>
2026-05-07 16:59:41 +0200

    fix lint
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- otelconf/x/generated_config.go
+++ otelconf/x/generated_config.go
@@ -1006,6 +1006,7 @@ type ExperimentalResourceDetector struct {
 }
 
 type ExperimentalAWSECSResourceDetector map[string]interface{}
+
 type ExperimentalServiceResourceDetector map[string]interface{}
 
 type ExperimentalSpanParent string
