fb99df248431450e901bbe757e8b9fcedf2cf6ad
Kathie Huang <kathie.huang@datadoghq.com>
2026-05-08 16:07:15 -0400

    detectors: add azurecontainerapps detector and unit tests

---
+++ detectors/azure/azurecontainerapps/detector.go
@@ -0,0 +1,53 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package azurecontainerapps provides a resource detector for Azure Container Apps.
+package azurecontainerapps // import "go.opentelemetry.io/contrib/detectors/azure/azurecontainerapps"
+
+import (
+	"context"
+	"os"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
+)
+
+// For a complete list of reserved environment variables in Azure Container Apps, see:
+// https://learn.microsoft.com/en-us/azure/container-apps/environment-variables?tabs=portal#built-in-environment-variables
+const (
+	containerAppNameEnvVar        = "CONTAINER_APP_NAME"
+	containerAppReplicaNameEnvVar = "CONTAINER_APP_REPLICA_NAME"
+)
+
+// ResourceDetector collects resource information from Azure Container Apps environment.
+type ResourceDetector struct{}
+
+// Compile time assertion that ResourceDetector implements the resource.Detector interface.
+var _ resource.Detector = (*ResourceDetector)(nil)
+
+// NewResourceDetector returns a resource detector that detects Azure Container Apps resources.
+func NewResourceDetector() resource.Detector {
+	return &ResourceDetector{}
+}
+
+// Detect collects resource attributes available when running on Azure Container Apps.
+// It returns an empty resource when not running on Azure Container Apps.
+func (*ResourceDetector) Detect(ctx context.Context) (*resource.Resource, error) {
+	appName := os.Getenv(containerAppNameEnvVar)
+	if appName == "" {
+		return resource.Empty(), nil
+	}
+
+	attrs := []attribute.KeyValue{
+		semconv.CloudProviderAzure,
+		semconv.CloudPlatformAzureContainerApps,
+		semconv.ServiceName(appName),
+	}
+
+	if replicaName := os.Getenv(containerAppReplicaNameEnvVar); replicaName != "" {
+		attrs = append(attrs, semconv.ServiceInstanceID(replicaName))
+	}
+
+	return resource.NewWithAttributes(semconv.SchemaURL, attrs...), nil
+}
---
+++ detectors/azure/azurecontainerapps/detector_test.go
@@ -0,0 +1,43 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package azurecontainerapps
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
+)
+
+// Successfully return resource when process is running in an Azure Container Apps environment.
+func TestDetectSuccess(t *testing.T) {
+	t.Setenv("CONTAINER_APP_NAME", "my-app")
+	t.Setenv("CONTAINER_APP_REPLICA_NAME", "my-app--abc123-0")
+
+	attributes := []attribute.KeyValue{
+		semconv.CloudProviderAzure,
+		semconv.CloudPlatformAzureContainerApps,
+		semconv.ServiceName("my-app"),
+		semconv.ServiceInstanceID("my-app--abc123-0"),
+	}
+	expectedResource := resource.NewWithAttributes(semconv.SchemaURL, attributes...)
+	detector := ResourceDetector{}
+	res, err := detector.Detect(t.Context())
+
+	assert.NoError(t, err, "Detector unexpectedly returned error")
+	assert.Equal(t, expectedResource, res, "Resource returned is incorrect")
+}
+
+// Return empty resource when not running in an Azure Container Apps environment.
+func TestReturnsIfNoEnvVars(t *testing.T) {
+	os.Clearenv()
+	detector := ResourceDetector{}
+	res, err := detector.Detect(t.Context())
+
+	assert.NoError(t, err)
+	assert.Equal(t, resource.Empty(), res)
+}
