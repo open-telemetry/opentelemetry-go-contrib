8c71d78a4cebb6f412d252e3cb7b991e4d16531e
Kathie Huang <kathie.huang@datadoghq.com>
2026-05-08 16:18:47 -0400

    document the detector

---
+++ detectors/azure/azurecontainerapps/doc.go
@@ -0,0 +1,19 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+Package azurecontainerapps provides a resource detector which supports
+detecting attributes specific to Azure Container Apps.
+
+According to semantic conventions for [cloud] and [service] attributes,
+the following attributes are added when running on Azure Container Apps:
+
+  - cloud.provider
+  - cloud.platform
+  - service.name
+  - service.instance.id
+
+[cloud]: https://github.com/open-telemetry/semantic-conventions/blob/main/docs/resource/cloud.md
+[service]: https://github.com/open-telemetry/semantic-conventions/blob/main/docs/resource/service.md
+*/
+package azurecontainerapps // import "go.opentelemetry.io/contrib/detectors/azure/azurecontainerapps"
