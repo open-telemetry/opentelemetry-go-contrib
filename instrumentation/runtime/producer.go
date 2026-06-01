c6b3fa544452b5bd49352fe2b1e2e34189965f04
David Ashpole <dashpole@google.com>
2026-06-01 09:17:49 -0400

    Apply suggestion from @pellared
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- instrumentation/runtime/producer.go
+++ instrumentation/runtime/producer.go
@@ -105,7 +105,7 @@ func convertRuntimeHistogram(runtimeHist *metrics.Float64Histogram, ts time.Time
 		count += c
 		// This computed sum is an underestimate, since it assumes each
 		// observation happens at the bucket's lower bound.
-		if i > 0 && count != 0 {
+		if i > 0 && c != 0 {
 			sum += bounds[i-1] * float64(c)
 		}
 	}
