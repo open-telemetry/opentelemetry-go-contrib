4219099d9f419567fe497e3cde798fde5d3a9304
Puneet Singh <puneet.mir@gmail.com>
2026-06-29 19:15:54 +0000

    fix semconv version

--- detectors/docker/detector_test.go
+++ detectors/docker/detector_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.41.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.42.0"
 )
 
 type mockProvider struct {
