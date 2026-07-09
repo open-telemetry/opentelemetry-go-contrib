a82f94f91dffad4400d14f6ddcde3ec5d4f4e89b
Puneet Singh <puneet.mir@gmail.com>
2026-07-09 23:26:43 +0530

    Update detectors/k8sapi/version.go
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- detectors/k8sapi/version.go
+++ detectors/k8sapi/version.go
@@ -4,14 +4,4 @@
 package k8sapi // import "go.opentelemetry.io/contrib/detectors/k8sapi"
 
 // Version is the current release version of the Kubernetes API resource detector.
-func Version() string {
-	return "0.16.0"
-	// This string is updated by the pre_release.sh script during release
-}
-
-// SemVersion is the semantic version to be supplied to tracer/meter creation.
-//
-// Deprecated: Use [Version] instead.
-func SemVersion() string {
-	return Version()
-}
+const Version = "0.16.0"
