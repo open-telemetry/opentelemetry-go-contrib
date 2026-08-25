2f20782f2c04cca11bbd0a35ba733056eecf4273
Robert Pajak <pellared@hotmail.com>
2026-08-25 21:55:12 +0200

    Prepare experimental-detectors for version v0.17.1

--- detectors/aws/elasticbeanstalk/version.go
+++ detectors/aws/elasticbeanstalk/version.go
@@ -4,4 +4,4 @@
 package elasticbeanstalk
 
 // Version is the current release version of the Amazon Elastic Beanstalk resource detector.
-const Version = "0.17.0"
+const Version = "0.17.1"
--- detectors/azure/azurecontainerapps/version.go
+++ detectors/azure/azurecontainerapps/version.go
@@ -4,4 +4,4 @@
 package azurecontainerapps
 
 // Version is the current release version of the Azure Container Apps resource detector.
-const Version = "0.17.0"
+const Version = "0.17.1"
